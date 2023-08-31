e961578784b9cf23bb4b843befb71fe964fc54e7
Marcos Candeia <marrcooos@gmail.com>
2023-08-31 09:32:34 -0300

    Deco hub App (#15)
    * Create decohub app
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add decohub app
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ decohub/mod.ts
@@ -0,0 +1,15 @@
+import type { App, FnContext } from "$live/mod.ts";
+import manifest, { Manifest } from "./manifest.gen.ts";
+
+// deno-lint-ignore ban-types
+export type State = {};
+/**
+ * @title Deco Hub
+ */
+export default function App(
+  state: State,
+): App<Manifest, State> {
+  return { manifest, state };
+}
+
+export type AppContext = FnContext<State, Manifest>;
