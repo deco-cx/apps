6ac85d642420ecc80fbf08ac40c513ccb715d90c
guitavano <tavano62@gmail.com>
2023-09-05 18:02:40 -0300

    fix description

--- coteminas/loaders/productList.ts
+++ coteminas/loaders/productList.ts
@@ -2,44 +2,44 @@ import { AppContext } from "../mod.ts";
 import type { Product } from "../../commerce/types.ts";
 import { toProduct } from "../utils/transform.ts";
 
-export interface Props{
+export interface Props {
   /**
    * @description Category name
    */
-    category: string;
+  category: string;
 
-    /**
-   * @description Category name
+  /**
+   * @description Quantity of items
    */
   count: number;
 }
-
 
 /**
  * @title Coteminas Integration
  * @description Product List loader
  */
-const productListLoader = async(
-    props: Props,
-    _req: Request,
-    ctx: AppContext
+const productListLoader = async (
+  props: Props,
+  _req: Request,
+  ctx: AppContext,
 ): Promise<Product[] | null> => {
-    const {api} = ctx
+  const { api } = ctx;
 
-    const {count = 8} = props
+  const { count = 8 } = props;
 
-    const response = await api["GET /api/product-catalog/resolve-route?path=/vm/:category"]({
-        category: props.category,
-        page: "",
+  const response = await api
+    ["GET /api/product-catalog/resolve-route?path=/vm/:category"]({
+      category: props.category,
+      page: "",
     }).then((res) => res.json());
 
-    const url = new URL(`https://${ctx.account}.com.br/`)
+  const url = new URL(`https://${ctx.account}.com.br/`);
 
-    const products = response.productCards.slice(0, count).map((product) => {
-        return toProduct(product, url)
-    })
+  const products = response.productCards.slice(0, count).map((product) => {
+    return toProduct(product, url);
+  });
 
-    return products
-}
+  return products;
+};
 
-export default productListLoader
+export default productListLoader;
