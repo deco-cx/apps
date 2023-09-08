0366e627d9380dd5a294c2699f0dd9b4143f4d0a
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2023-09-08 15:17:40 -0300

    Add a matcher to negate a matcher.

---
+++ website/matchers/negate.ts
@@ -0,0 +1,20 @@
+import { MatchContext, Matcher } from "deco/blocks/matcher.ts";
+
+/**
+ * @title Combined options with {{{op}}}
+ */
+export interface Props {
+  op: "or" | "and";
+  matchers: Matcher[];
+}
+
+/**
+ * @title OR & AND Matcher
+ */
+const MatchMulti = ({ op, matchers }: Props) => (ctx: MatchContext) => {
+  return op === "or"
+    ? matchers.some((matcher) => matcher(ctx))
+    : matchers.every((matcher) => matcher(ctx));
+};
+
+export default MatchMulti;
