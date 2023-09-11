bc6519823664247d337c167ae0820a750b97f9cc
Marcos Candeia <marrcooos@gmail.com>
2023-09-11 13:03:43 -0300

    Add verified-reviews to decohub
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ decohub/apps/verified-reviews.ts
@@ -0,0 +1,1 @@
+export { default } from "../../verified-reviews/mod.ts";
--- decohub/manifest.gen.ts
+++ decohub/manifest.gen.ts
@@ -2,13 +2,15 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/handlebars.ts";
-import * as $$$$$$$$$$$1 from "./apps/workflows.ts";
+import * as $$$$$$$$$$$0 from "./apps/workflows.ts";
+import * as $$$$$$$$$$$1 from "./apps/handlebars.ts";
+import * as $$$$$$$$$$$2 from "./apps/verified-reviews.ts";
 
 const manifest = {
   "apps": {
-    "decohub/apps/handlebars.ts": $$$$$$$$$$$0,
-    "decohub/apps/workflows.ts": $$$$$$$$$$$1,
+    "decohub/apps/handlebars.ts": $$$$$$$$$$$1,
+    "decohub/apps/verified-reviews.ts": $$$$$$$$$$$2,
+    "decohub/apps/workflows.ts": $$$$$$$$$$$0,
   },
   "name": "decohub",
   "baseUrl": import.meta.url,
