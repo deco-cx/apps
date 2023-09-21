0533157821e9f77a21a81931f15d19c37050960a
Marcos Candeia <marrcooos@gmail.com>
2023-09-21 15:27:58 -0300

    Defer page resolve (#71)
    * Defer page resolve
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Fix router redirects
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update deco version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- website/handlers/fresh.ts
+++ website/handlers/fresh.ts
@@ -1,5 +1,10 @@
 import { HandlerContext } from "$fresh/server.ts";
 import { Page } from "deco/blocks/page.ts";
+import {
+  asResolved,
+  BaseContext,
+  isDeferred,
+} from "deco/engine/core/resolver.ts";
 import { DecoState } from "deco/types.ts";
 import { allowCorsFor } from "deco/utils/http.ts";
 import { ConnInfo } from "std/http/server.ts";
@@ -21,15 +26,22 @@ export const isFreshCtx = <TState>(
  * @title Fresh Page
  * @description Renders a fresh page.
  */
-export default function Fresh(page: FreshConfig) {
+export default function Fresh(freshConfig: FreshConfig) {
   return async (req: Request, ctx: ConnInfo) => {
+    if (req.method === "HEAD") {
+      return new Response(null, { status: 200 });
+    }
+    const page =
+      isDeferred<Page, BaseContext & { context: ConnInfo }>(freshConfig.page)
+        ? await freshConfig.page({ context: ctx })
+        : freshConfig.page;
     const url = new URL(req.url);
     if (url.searchParams.get("asJson") !== null) {
       return Response.json(page, { headers: allowCorsFor(req) });
     }
     if (isFreshCtx<DecoState>(ctx)) {
       return await ctx.render({
-        ...page,
+        page,
         routerInfo: {
           flags: ctx.state.flags,
           pagePath: ctx.state.pathTemplate,
@@ -41,3 +53,12 @@ export default function Fresh(page: FreshConfig) {
     });
   };
 }
+
+export const onBeforeResolveProps = (
+  props: FreshConfig,
+) => {
+  if (props?.page) {
+    return { ...props, page: asResolved(props.page, true) };
+  }
+  return props;
+};
