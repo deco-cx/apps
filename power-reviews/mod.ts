5282cd97321861331fa6700b53d7ede512fd8cce
guitavano <tavano62@gmail.com>
2023-09-22 10:22:02 -0300

    readme

--- power-reviews/mod.ts
+++ power-reviews/mod.ts
@@ -1,54 +1,51 @@
 import type { App, FnContext } from "deco/mod.ts";
 
-import { createHttpClient } from "../utils/http.ts"
+import { createHttpClient } from "../utils/http.ts";
 import { PowerReviews } from "./utils/client.ts";
 import { fetchSafe } from "../utils/fetch.ts";
 import manifest, { Manifest } from "./manifest.gen.ts";
 
-
 export type AppContext = FnContext<State, Manifest>;
 
-export interface Props{
-    /**
-     * @title App Key
-     */
-    appKey: string;
-    /**
-     * @title Merchant Id
-     */
-    merchantId: string;
-    /**
-     * @title Merchant Group
-     */
-    merchantGroup: string;
-
+export interface Props {
+  /**
+   * @title App Key
+   */
+  appKey: string;
+  /**
+   * @title Merchant Id
+   */
+  merchantId: string;
+  /**
+   * @title Merchant Group
+   */
+  merchantGroup: string;
 }
 
 interface State extends Props {
-    api: ReturnType<typeof createHttpClient<PowerReviews>>;
-    apiWrite: ReturnType<typeof createHttpClient<PowerReviews>>;
+  api: ReturnType<typeof createHttpClient<PowerReviews>>;
+  apiWrite: ReturnType<typeof createHttpClient<PowerReviews>>;
 }
 
 export default function App(state: Props): App<Manifest, State> {
+  const api = createHttpClient<PowerReviews>({
+    base: `https://readservices-b2c.powerreviews.com`,
+    fetcher: fetchSafe,
+    headers: new Headers({
+      Authorization: "6b533d2d-fa46-4c49-baac-34b69240ede7",
+    }),
+  });
 
-    const api = createHttpClient<PowerReviews>({
-        base: `https://readservices-b2c.powerreviews.com`,
-        fetcher: fetchSafe,
-        headers: new Headers({
-            Authorization: "6b533d2d-fa46-4c49-baac-34b69240ede7"
-        }),
-    });
+  const apiWrite = createHttpClient<PowerReviews>({
+    base: `https://writeservices.powerreviews.com`,
+    fetcher: fetchSafe,
+    headers: new Headers({
+      Authorization: "6b533d2d-fa46-4c49-baac-34b69240ede7",
+    }),
+  });
 
-    const apiWrite = createHttpClient<PowerReviews>({
-        base: `https://writeservices.powerreviews.com`,
-        fetcher: fetchSafe,
-        headers: new Headers({
-            Authorization: "6b533d2d-fa46-4c49-baac-34b69240ede7"
-        }),
-    });
-
-    return {
-        state: { ...state, api, apiWrite },
-        manifest,
-    };
+  return {
+    state: { ...state, api, apiWrite },
+    manifest,
+  };
 }
