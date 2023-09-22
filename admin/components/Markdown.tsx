a0f995d96e5e7d6f6ce90142fd6a0e7797a17de8
gimenes <tlgimenes@gmail.com>
2023-09-22 10:25:46 -0300

    wip

--- admin/components/Markdown.tsx
+++ admin/components/Markdown.tsx
@@ -1,5 +1,5 @@
 import { fromFileUrl } from "std/path/mod.ts";
-import { CSS, render } from "https://deno.land/x/gfm@0.1.23/mod.ts";
+import { CSS, KATEX_CSS, render } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
@@ -11,11 +11,16 @@ export const Markdown = (path: string) => () => {
   return (
     <>
       <style dangerouslySetInnerHTML={{ __html: CSS }} />
+      <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
       <div
         class="markdown-body"
         style={{ padding: "16px 32px" }}
         dangerouslySetInnerHTML={{
-          __html: render(content, { allowIframes: true }),
+          __html: render(content, {
+            allowIframes: true,
+            allowMath: true,
+            disableHtmlSanitization: true,
+          }),
         }}
       />
     </>
