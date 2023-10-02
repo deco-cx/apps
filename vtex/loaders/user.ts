806ae37236241f91575bbd51f524527d5e29cdfe
Jonathan Lopes <jonathanwlopes93@gmail.com>
2023-10-02 13:27:05 -0300

    feat: return profile user

--- vtex/loaders/user.ts
+++ vtex/loaders/user.ts
@@ -1,22 +1,36 @@
-import { AppContext } from "../mod.ts";
-import { parseCookie } from "../utils/vtexId.ts";
+import { AppContext } from "../mod.ts"
+import { parseCookie } from "../utils/vtexId.ts"
 
 export interface User {
-  id: string;
-  email: string;
+  id: string
+  email: string
+  firstName?: string
+  lastName?: string
+  profilePicture?: string
+  gender?: string
 }
 
-function loader(_props: unknown, req: Request, ctx: AppContext): User | null {
-  const { payload } = parseCookie(req.headers, ctx.account);
+async function loader(
+  _props: unknown,
+  req: Request,
+  ctx: AppContext
+): Promise<User | null> {
+  const { io } = ctx
+  const { cookie, payload } = parseCookie(req.headers, ctx.account)
 
   if (!payload?.sub || !payload?.userId) {
-    return null;
+    return null
   }
 
-  return {
-    id: payload.userId,
-    email: payload.sub,
-  };
+  const query =
+    "query getUserProfile { profile { id email firstName lastName profilePicture gender }}"
+
+  try {
+    const user = await io.query({ query }, { headers: { cookie } })
+    return { ...user.profile } as User
+  } catch (_) {
+    return null
+  }
 }
 
-export default loader;
+export default loader
