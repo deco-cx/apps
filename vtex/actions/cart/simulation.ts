5f9dc7bcda1cee377f20c97bad881f491fee022e
Luis Sousa <lv-ss@hotmail.com>
2023-10-04 16:47:55 -0300

    add RnbBehavior to vtex cart simulation (#90)

--- vtex/actions/cart/simulation.ts
+++ vtex/actions/cart/simulation.ts
@@ -11,6 +11,7 @@ export interface Props {
   items: Item[];
   postalCode: string;
   country: string;
+  RnbBehavior?: 0 | 1;
 }
 
 /**
@@ -23,24 +24,23 @@ const action = async (
 ): Promise<SimulationOrderForm> => {
   const cookie = req.headers.get("cookie") ?? "";
   const { vcsDeprecated } = ctx;
-  const {
-    items,
-    postalCode,
-    country,
-  } = props;
+  const { items, postalCode, country, RnbBehavior = 1 } = props;
 
-  const response = await vcsDeprecated
-    ["POST /api/checkout/pub/orderForms/simulation"](
-      {},
-      {
-        body: { items, postalCode, country },
-        headers: {
-          accept: "application/json",
-          "content-type": "application/json",
-          cookie,
-        },
+  const response = await vcsDeprecated[
+    "POST /api/checkout/pub/orderForms/simulation"
+  ](
+    {
+      RnbBehavior,
+    },
+    {
+      body: { items, country, postalCode },
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        cookie,
       },
-    );
+    },
+  );
 
   return response.json();
 };
