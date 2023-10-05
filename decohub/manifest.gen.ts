a88d8baee9d0edbea6cdf4859711f7be0d12c8f8
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-10-05 20:30:31 -0300

    chore: rebase

--- decohub/manifest.gen.ts
+++ decohub/manifest.gen.ts
@@ -10,22 +10,24 @@ import * as $$$$$$$$$$$4 from "./apps/linx.ts";
 import * as $$$$$$$$$$$5 from "./apps/verified-reviews.ts";
 import * as $$$$$$$$$$$6 from "./apps/algolia.ts";
 import * as $$$$$$$$$$$7 from "./apps/typesense.ts";
-import * as $$$$$$$$$$$8 from "./apps/vnda.ts";
-import * as $$$$$$$$$$$9 from "./apps/admin.ts";
-import * as $$$$$$$$$$$10 from "./apps/handlebars.ts";
-import * as $$$$$$$$$$$11 from "./apps/power-reviews.ts";
+import * as $$$$$$$$$$$8 from "./apps/analytics.ts";
+import * as $$$$$$$$$$$9 from "./apps/vnda.ts";
+import * as $$$$$$$$$$$10 from "./apps/admin.ts";
+import * as $$$$$$$$$$$11 from "./apps/handlebars.ts";
+import * as $$$$$$$$$$$12 from "./apps/power-reviews.ts";
 
 const manifest = {
   "apps": {
-    "decohub/apps/admin.ts": $$$$$$$$$$$9,
+    "decohub/apps/admin.ts": $$$$$$$$$$$10,
     "decohub/apps/algolia.ts": $$$$$$$$$$$6,
-    "decohub/apps/handlebars.ts": $$$$$$$$$$$10,
+    "decohub/apps/analytics.ts": $$$$$$$$$$$8,
+    "decohub/apps/handlebars.ts": $$$$$$$$$$$11,
     "decohub/apps/linx.ts": $$$$$$$$$$$4,
-    "decohub/apps/power-reviews.ts": $$$$$$$$$$$11,
+    "decohub/apps/power-reviews.ts": $$$$$$$$$$$12,
     "decohub/apps/shopify.ts": $$$$$$$$$$$2,
     "decohub/apps/typesense.ts": $$$$$$$$$$$7,
     "decohub/apps/verified-reviews.ts": $$$$$$$$$$$5,
-    "decohub/apps/vnda.ts": $$$$$$$$$$$8,
+    "decohub/apps/vnda.ts": $$$$$$$$$$$9,
     "decohub/apps/vtex.ts": $$$$$$$$$$$0,
     "decohub/apps/wake.ts": $$$$$$$$$$$1,
     "decohub/apps/workflows.ts": $$$$$$$$$$$3,
