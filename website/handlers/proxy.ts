b49441ca22fb34bf8e8ce7ed6b44b6b2211ac48c
Nathan Luiz <nathanluiz33@gmail.com>
2023-10-08 03:32:37 -0300

    adicionando script e link no head do html

--- website/handlers/proxy.ts
+++ website/handlers/proxy.ts
@@ -2,6 +2,8 @@ import { isFreshCtx } from "deco/handlers/fresh.ts";
 import { DecoSiteState } from "deco/mod.ts";
 import { Handler } from "std/http/mod.ts";
 import { proxySetCookie } from "../../utils/cookie.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
 
 const HOP_BY_HOP = [
   "Keep-Alive",
@@ -63,13 +65,44 @@ async (req, _ctx) => {
   for (const { key, value } of customHeaders) {
     headers.set(key, value);
   }
-
+  
   const response = await fetch(to, {
     headers,
     redirect: "manual",
     method: req.method,
     body: req.body,
   });
+
+  const contentType = response.headers.get("Content-Type");
+
+  let newBody = null;
+
+  if (contentType?.includes("text/html")) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(await response.text (), 'text/html');
+    
+    const scriptElement = doc?.createElement("script");
+    scriptElement?.setAttribute("data-exclude", "/proxy");
+    scriptElement?.setAttribute("data-api", "https://plausible.io/api/event");
+
+    const linkElement = doc?.createElement("link");
+    linkElement?.setAttribute("rel", "dns-prefetch");
+    linkElement?.setAttribute("href", "https://plausible.io/api/event");
+
+    const linkElement2 = doc?.createElement("link");
+    linkElement2?.setAttribute("rel", "preconnect");
+    linkElement2?.setAttribute("href", "https://plausible.io/api/event");
+    linkElement2?.setAttribute("crossorigin", "anonymous");
+
+    const plausibleCodeScript = '!function(){"use strict";var l=window.location,s=window.document,u=s.currentScript,c=u.getAttribute("data-api")||new URL(u.src).origin+"/api/event";function p(t,e){t&&console.warn("Ignoring Event: "+t),e&&e.callback&&e.callback()}function t(t,e){if(/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(l.hostname)||"file:"===l.protocol)return p("localhost",e);if(window._phantom||window.__nightmare||window.navigator.webdriver||window.Cypress)return p(null,e);try{if("true"===window.localStorage.plausible_ignore)return p("localStorage flag",e)}catch(t){}var i=u&&u.getAttribute("data-include"),n=u&&u.getAttribute("data-exclude");if("pageview"===t){i=!i||i.split(",").some(a),n=n&&n.split(",").some(a);if(!i||n)return p("exclusion rule",e)}function a(t){return l.pathname.match(new RegExp("^"+t.trim().replace(/\*\*/g,".*").replace(/([^\.])\*/g,"$1[^\\s/]*")+"/?$"))}var i={},n=(i.n=t,i.u=l.href,i.d=((w,d)=>{const h=w.location.hostname;return h.replace(/^www./,"")})(window,document),i.r=s.referrer||null,e&&e.meta&&(i.m=JSON.stringify(e.meta)),e&&e.props&&(i.p=e.props),u.getAttributeNames().filter(function(t){return"event-"===t.substring(0,6)})),r=i.p||{},o=(n.forEach(function(t){var e=t.replace("event-",""),t=u.getAttribute(t);r[e]=r[e]||t}),i.p=r,new XMLHttpRequest);o.open("POST",c,!0),o.setRequestHeader("Content-Type","text/plain"),o.send(JSON.stringify(i)),o.onreadystatechange=function(){4===o.readyState&&e&&e.callback&&e.callback()}}var e=window.plausible&&window.plausible.q||[];window.plausible=t;for(var i,n=0;n<e.length;n++)t.apply(this,e[n]);function a(){i!==l.pathname&&(i=l.pathname,t("pageview"))}var r,o=window.history;o.pushState&&(r=o.pushState,o.pushState=function(){r.apply(this,arguments),a()},window.addEventListener("popstate",a)),"prerender"===s.visibilityState?s.addEventListener("visibilitychange",function(){i||"visible"!==s.visibilityState||a()}):a()}();';
+    if (doc !== null) {
+      scriptElement?.appendChild(doc.createTextNode(plausibleCodeScript));
+      if (scriptElement !== undefined) doc.head.appendChild(scriptElement);
+      if (linkElement !== undefined) doc.head.appendChild(linkElement);
+      if (linkElement2 !== undefined) doc.head.appendChild(linkElement2);
+    }
+    newBody = new TextEncoder().encode(doc?.documentElement?.outerHTML);
+  }
 
   // Change cookies domain
   const responseHeaders = new Headers(response.headers);
@@ -86,8 +119,8 @@ async (req, _ctx) => {
       );
     }
   }
-
-  return new Response(response.body, {
+  
+  return new Response((newBody === null ? response.body : newBody), {
     status: response.status,
     headers: responseHeaders,
   });
