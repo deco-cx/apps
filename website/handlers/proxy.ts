5b32c5a69a19b1a68952f484f1f43e08a6c0987a
Nathan Luiz <nathanluiz33@gmail.com>
2023-10-10 10:07:42 -0300

    fixing PR

--- website/handlers/proxy.ts
+++ website/handlers/proxy.ts
@@ -86,26 +86,25 @@ async (req, _ctx) => {
     const plausibleScript =
       '<script data-exclude="/proxy" data-api="https://plausible.io/api/event">!function(){"use strict";var l=window.location,s=window.document,u=s.currentScript,c=u.getAttribute("data-api")||new URL(u.src).origin+"/api/event";function p(t,e){t&&console.warn("Ignoring Event: "+t),e&&e.callback&&e.callback()}function t(t,e){if(/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(l.hostname)||"file:"===l.protocol)return p("localhost",e);if(window._phantom||window.__nightmare||window.navigator.webdriver||window.Cypress)return p(null,e);try{if("true"===window.localStorage.plausible_ignore)return p("localStorage flag",e)}catch(t){}var i=u&&u.getAttribute("data-include"),n=u&&u.getAttribute("data-exclude");if("pageview"===t){i=!i||i.split(",").some(a),n=n&&n.split(",").some(a);if(!i||n)return p("exclusion rule",e)}function a(t){return l.pathname.match(new RegExp("^"+t.trim().replace(/\*\*/g,".*").replace(/([^\.])\*/g,"$1[^\\s/]*")+"/?$"))}var i={},n=(i.n=t,i.u=l.href,i.d=((w,d)=>{const h=w.location.hostname;return h.replace(/^www./,"")})(window,document),i.r=s.referrer||null,e&&e.meta&&(i.m=JSON.stringify(e.meta)),e&&e.props&&(i.p=e.props),u.getAttributeNames().filter(function(t){return"event-"===t.substring(0,6)})),r=i.p||{},o=(n.forEach(function(t){var e=t.replace("event-",""),t=u.getAttribute(t);r[e]=r[e]||t}),i.p=r,new XMLHttpRequest);o.open("POST",c,!0),o.setRequestHeader("Content-Type","text/plain"),o.send(JSON.stringify(i)),o.onreadystatechange=function(){4===o.readyState&&e&&e.callback&&e.callback()}}var e=window.plausible&&window.plausible.q||[];window.plausible=t;for(var i,n=0;n<e.length;n++)t.apply(this,e[n]);function a(){i!==l.pathname&&(i=l.pathname,t("pageview"))}var r,o=window.history;o.pushState&&(r=o.pushState,o.pushState=function(){r.apply(this,arguments),a()},window.addEventListener("popstate",a)),"prerender"===s.visibilityState?s.addEventListener("visibilitychange",function(){i||"visible"!==s.visibilityState||a()}):a()}();</script>';
 
-    let cur = "aaaaaa";
+    let accHtml: string | undefined = "";
     const insertPlausible = new TransformStream({
       async transform(chunk, controller) {
-        chunk = await chunk;
-        let newChunk = chunk;
-        if (cur != "") {
+        let newChunk = await chunk;
+        if (accHtml != undefined) {
           for (let i = 0; i < chunk.length; i++) {
-            cur = cur.slice(1, 6) + decoder.decode(chunk.slice(i, i + 1));
+            accHtml = accHtml.slice(-5) + decoder.decode(chunk.slice(i, i + 1));
+            
+            if (accHtml == "<head>") {
+              accHtml = "";
 
-            if (cur == "<head>") {
-              cur = "";
+              accHtml += decoder.decode(chunk.slice(0, i + 1));
+              accHtml += link1;
+              accHtml += link2;
+              accHtml += plausibleScript;
+              accHtml += decoder.decode(chunk.slice(i + 1, chunk.length));
 
-              cur += decoder.decode(chunk.slice(0, i + 1));
-              cur += link1;
-              cur += link2;
-              cur += plausibleScript;
-              cur += decoder.decode(chunk.slice(i + 1, chunk.length));
-
-              newChunk = encoder.encode(cur);
-              cur = "";
+              newChunk = encoder.encode(accHtml);
+              accHtml = undefined;
               break;
             }
           }
