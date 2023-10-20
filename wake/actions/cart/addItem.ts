dea43448b047ce4bf10e43a5caf6ded4f59b8fa9
Luis Sousa <lv-ss@hotmail.com>
2023-10-20 13:47:51 -0300

    fix: loader call

--- wake/actions/cart/addItem.ts
+++ wake/actions/cart/addItem.ts
@@ -1,15 +1,15 @@
 import { AppContext } from "../../mod.ts";
 import { CheckoutFragment } from "../../utils/graphql/storefront.graphql.gen.ts";
-import AddItems, { CartItem as Props } from "./addItems.ts";
+import { CartItem as Props } from "./addItems.ts";
 
 export type { CartItem as Props } from "./addItems.ts";
 
 const action = async (
   props: Props,
-  req: Request,
+  _req: Request,
   ctx: AppContext,
 ): Promise<Partial<CheckoutFragment>> => {
-  return await ctx.invoke.wake.actions.cart.addItem({ products: [props] });
+  return await ctx.invoke.wake.actions.cart.addItems({ products: [props] });
 };
 
 export default action;
