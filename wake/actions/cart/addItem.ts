024dac296a4a562a6c75d544196111163a61881e
Luis Sousa <lv-ss@hotmail.com>
2023-10-20 16:10:11 -0300

    Action add multiple items to cart and params from props on PLP (#147)
    * wake
    
    * wake
    
    * feat: wake add multiple items
    
    * Update wake/actions/cart/addItem.ts
    
    Co-authored-by: Marcos Candeia <marrcooos@gmail.com>
    
    * fix: loader call
    
    * fix: search query param
    
    ---------
    
    Co-authored-by: Marcos Candeia <marrcooos@gmail.com>

--- wake/actions/cart/addItem.ts
+++ wake/actions/cart/addItem.ts
@@ -1,44 +1,15 @@
-import { HttpError } from "../../../utils/http.ts";
 import { AppContext } from "../../mod.ts";
-import { getCartCookie, setCartCookie } from "../../utils/cart.ts";
-import { AddItemToCart } from "../../utils/graphql/queries.ts";
-import {
-  AddItemToCartMutation,
-  AddItemToCartMutationVariables,
-  CheckoutFragment,
-} from "../../utils/graphql/storefront.graphql.gen.ts";
+import { CheckoutFragment } from "../../utils/graphql/storefront.graphql.gen.ts";
+import { CartItem as Props } from "./addItems.ts";
 
-export interface Props {
-  productVariantId: number;
-  quantity: number;
-  customization: { customizationId: number; value: string }[];
-  subscription: { subscriptionGroupId: number; recurringTypeId: number };
-}
+export type { CartItem as Props } from "./addItems.ts";
 
 const action = async (
   props: Props,
-  req: Request,
+  _req: Request,
   ctx: AppContext,
 ): Promise<Partial<CheckoutFragment>> => {
-  const { storefront } = ctx;
-  const cartId = getCartCookie(req.headers);
-
-  if (!cartId) {
-    throw new HttpError(400, "Missing cart cookie");
-  }
-
-  const data = await storefront.query<
-    AddItemToCartMutation,
-    AddItemToCartMutationVariables
-  >({
-    variables: { input: { id: cartId, products: [props] } },
-    ...AddItemToCart,
-  });
-
-  const checkoutId = data.checkout?.checkoutId;
-  setCartCookie(ctx.response.headers, checkoutId);
-
-  return data.checkout ?? {};
+  return await ctx.invoke.wake.actions.cart.addItems({ products: [props] });
 };
 
 export default action;
