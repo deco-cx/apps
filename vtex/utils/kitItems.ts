ff538dffc5c086208b5a5124857fd568da7dfe50
Jonathan Lopes <jonathanwlopes93@gmail.com>
2023-10-20 11:52:43 -0300

    fix: 'data' is of type 'unknown'

--- vtex/utils/kitItems.ts
+++ vtex/utils/kitItems.ts
@@ -1,11 +1,12 @@
 import { AppContext } from "../mod.ts";
 import { QUERY_KIT_ITEMS } from "./graphql/kitItems.ts";
+import { Product } from "./types.ts";
 
 export const getKitItems = async (sku: string, ctx: AppContext) => {
   const { io } = ctx;
 
   try {
-    const data = await io.query(
+    const data: { product: Product } = await io.query(
       {
         query: QUERY_KIT_ITEMS,
         variables: {
