a2e9a65b5b003bf2bc2d66298ab96c22b775dde6
gimenes <tlgimenes@gmail.com>
2023-10-24 11:50:09 -0300

    use person instead of user

--- vtex/loaders/user.ts
+++ vtex/loaders/user.ts
@@ -1,39 +1,49 @@
-import { AppContext } from "../mod.ts"
-import { parseCookie } from "../utils/vtexId.ts"
+import { Person } from "../../commerce/types.ts";
+import { AppContext } from "../mod.ts";
+import { parseCookie } from "../utils/vtexId.ts";
 
-export interface User {
-  id: string
-  email: string
-  firstName?: string
-  lastName?: string
-  profilePicture?: string
-  gender?: string
+interface User {
+  id: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  profilePicture?: string;
+  gender?: string;
 }
 
 async function loader(
   _props: unknown,
   req: Request,
-  ctx: AppContext
-): Promise<User | null> {
-  const { io } = ctx
-  const { cookie, payload } = parseCookie(req.headers, ctx.account)
+  ctx: AppContext,
+): Promise<Person | null> {
+  const { io } = ctx;
+  const { cookie, payload } = parseCookie(req.headers, ctx.account);
 
   if (!payload?.sub || !payload?.userId) {
-    return null
+    return null;
   }
 
   const query =
-    "query getUserProfile { profile { id email firstName lastName profilePicture gender }}"
+    "query getUserProfile { profile { id email firstName lastName profilePicture gender }}";
 
   try {
-    const user: { profile: User } = await io.query(
+    const { profile: user } = await io.query<{ profile: User }, null>(
       { query },
-      { headers: { cookie } }
-    )
-    return { ...user.profile }
+      { headers: { cookie } },
+    );
+
+    return {
+      "@id": user.id,
+      email: user.email,
+      givenName: user.firstName,
+      familyName: user.lastName,
+      gender: user.gender === "f"
+        ? "https://schema.org/Female"
+        : "https://schema.org/Male",
+    };
   } catch (_) {
-    return null
+    return null;
   }
 }
 
-export default loader
+export default loader;
