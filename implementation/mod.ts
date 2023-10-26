ed84225665329a164e1ac3cbb36628424538cea6
Lucis <luciannojunior@gmail.com>
2023-10-26 12:00:09 -0300

    Adding implementation app (#167)
    * Adding implementation app
    
    * Sorting list

---
+++ implementation/mod.ts
@@ -0,0 +1,58 @@
+import type { App, AppContext as AC } from "deco/mod.ts";
+import manifest, { Manifest } from "./manifest.gen.ts";
+
+export type Agency =
+  | "2B Digital"
+  | "Adaptio"
+  | "Agência E-plus"
+  | "Agência MKT Now"
+  | "Agência N1"
+  | "Allfa"
+  | "Allomni"
+  | "Auaha"
+  | "Avanti"
+  | "B8One"
+  | "Betminds"
+  | "Box Ideias"
+  | "Codeblue"
+  | "Codeby"
+  | "Corebiz"
+  | "Econverse"
+  | "ED3"
+  | "Eficaz Marketing"
+  | "Four2One"
+  | "FRN Comunicação"
+  | "Iryasolutions"
+  | "Keyrus"
+  | "M3Ecommerce"
+  | "Maeztra"
+  | "Quality Digital"
+  | "Quickdigital"
+  | "Saga Partners"
+  | "Social Commerce"
+  | "Tec4Udigital"
+  | "The Goal - Agência Cross-Commerce"
+  | "Thealfred"
+  | "Wave Commerce"
+  | "Wecode"
+  | "Wedigi"
+  | "Wicomm";
+
+export interface State {
+  serviceProvider?: Agency;
+  /**
+   * @format date
+   */
+  expectedGoLive?: string;
+}
+
+/**
+ * @title implementation
+ */
+export default function App(
+  state: State,
+): App<Manifest, State> {
+  return { manifest, state };
+}
+
+export type AppContext = AC<ReturnType<typeof App>>;
