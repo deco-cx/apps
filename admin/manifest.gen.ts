3cfab9c121fd752ccd07ff7514ca158305c7efe6
Héricles <hericles.me@icloud.com>
2023-10-28 20:49:47 -0300

    feat: add pages list loader

--- admin/manifest.gen.ts
+++ admin/manifest.gen.ts
@@ -8,6 +8,7 @@ import * as $$$2 from "./loaders/blocks/latest.ts";
 import * as $$$3 from "./loaders/blocks/listRevisions.ts";
 import * as $$$4 from "./loaders/state.ts";
 import * as $$$5 from "./loaders/releases/blocks.ts";
+import * as $$$6 from "./loaders/pages/list.ts";
 import * as $$$$$$$$$0 from "./actions/blocks/publish.ts";
 import * as $$$$$$$$$1 from "./actions/blocks/restore.ts";
 import * as $$$$$$$$$2 from "./actions/blocks/safeDelete.ts";
@@ -21,6 +22,7 @@ const manifest = {
     "deco-sites/admin/loaders/blocks/listRevisions.ts": $$$3,
     "deco-sites/admin/loaders/blocks/published.ts": $$$1,
     "deco-sites/admin/loaders/blocks/revision.ts": $$$0,
+    "deco-sites/admin/loaders/pages/list.ts": $$$6,
     "deco-sites/admin/loaders/releases/blocks.ts": $$$5,
     "deco-sites/admin/loaders/state.ts": $$$4,
   },
