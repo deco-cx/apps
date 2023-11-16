6a50aed02bd06e7f8e98791cb66a63cb34ad73d4
Nathan Luiz <nathanluiz33@gmail.com>
2023-11-16 10:15:51 -0300

    adding svg

--- utils/defaultErrorPage.tsx
+++ utils/defaultErrorPage.tsx
@@ -150,12 +150,21 @@ export default function ErrorPageComponent({ error }: Props) {
         <div class="media:flex media:flex-col pt-20">
           <div class={`flex justify-center ${error ? "" : "pt-36"}`}>
             <svg
-              width={120}
-              height={60}
-              strokeWidth={2}
-              class="relative"
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-alert-circle"
+              width="55"
+              height="55"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              <use href={asset(`/sprites.svg#alert-circle`)} />
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+              <path d="M12 8v4" />
+              <path d="M12 16h.01" />
             </svg>
           </div>
           <div class="pt-4 pb-[15px] text-center">
