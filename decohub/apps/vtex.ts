be09516716789409181fc0fe1a24581ce7d56138
guitavano <tavano62@gmail.com>
2023-12-12 12:48:50 -0300

    fix typescript

--- decohub/apps/vtex.ts
+++ decohub/apps/vtex.ts
@@ -3,10 +3,8 @@ export { default } from "../../vtex/mod.ts";
 import { AppRuntime } from "deco/types.ts";
 import { PreviewVtex } from "../../vtex/preview/Preview.tsx";
 import { Markdown } from "../components/Markdown.tsx";
-import { BaseContext } from "deco/engine/core/resolver.ts";
-import { App } from "../../vtex/mod.ts";
 
-export const preview = async (props: AppRuntime<BaseContext, App["state"]>) => {
+export const preview = async (props: AppRuntime) => {
   const markdownContent = await Markdown(
     new URL("../../vtex/README.md", import.meta.url).href,
   );
