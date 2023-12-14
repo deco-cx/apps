dccb50dba90dbc70a1c0c2a8fe36424f4e4f7c72
guitavano <tavano62@gmail.com>
2023-12-14 11:41:19 -0300

    checl

--- nuvemshop/manifest.gen.ts
+++ nuvemshop/manifest.gen.ts
@@ -2,22 +2,22 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from "./loaders/proxy.ts";
-import * as $$$1 from "./loaders/cart.ts";
-import * as $$$2 from "./loaders/productList.ts";
-import * as $$$3 from "./loaders/productDetailsPage.ts";
-import * as $$$4 from "./loaders/productListingPage.ts";
+import * as $$$0 from "./loaders/productList.ts";
+import * as $$$1 from "./loaders/productDetailsPage.ts";
+import * as $$$2 from "./loaders/productListingPage.ts";
+import * as $$$3 from "./loaders/proxy.ts";
+import * as $$$4 from "./loaders/cart.ts";
 import * as $$$$0 from "./handlers/sitemap.ts";
 import * as $$$$$$$$$0 from "./actions/cart/updateItems.ts";
 import * as $$$$$$$$$1 from "./actions/cart/addItems.ts";
 
 const manifest = {
   "loaders": {
-    "nuvemshop/loaders/cart.ts": $$$1,
-    "nuvemshop/loaders/productDetailsPage.ts": $$$3,
-    "nuvemshop/loaders/productList.ts": $$$2,
-    "nuvemshop/loaders/productListingPage.ts": $$$4,
-    "nuvemshop/loaders/proxy.ts": $$$0,
+    "nuvemshop/loaders/cart.ts": $$$4,
+    "nuvemshop/loaders/productDetailsPage.ts": $$$1,
+    "nuvemshop/loaders/productList.ts": $$$0,
+    "nuvemshop/loaders/productListingPage.ts": $$$2,
+    "nuvemshop/loaders/proxy.ts": $$$3,
   },
   "handlers": {
     "nuvemshop/handlers/sitemap.ts": $$$$0,
