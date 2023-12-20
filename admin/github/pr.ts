92b8ca0c168c6839356c9ae9b7d59266a5308ba4
Marcos Candeia <marrcooos@gmail.com>
2023-12-20 19:17:14 -0300

    Do not always build prod
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- admin/github/pr.ts
+++ admin/github/pr.ts
@@ -1,35 +1,12 @@
 import { GithubEventListener } from "../mod.ts";
-import { controllerFor } from "./statusController.ts";
+import { handleChange } from "./handler.ts";
 
 export const prEventHandler: GithubEventListener<"pull_request"> = {
   events: ["pull_request"],
   handle: async (event, ctx) => {
-    const { actions, loaders } = ctx.invoke["deco-sites/admin"];
     const owner = event.repository.owner.login;
     const repo = event.repository.name;
     const commitSha = event.pull_request.head.sha;
-    const site = repo; // TODO(mcandeia) it should have a way to deploy more sites to the same repo
-    const context = `(beta) Deco / sites-${site} / preview`;
-    const statusController = controllerFor({
-      owner,
-      repo,
-      commitSha,
-      context,
-    }, ctx);
-    try {
-      statusController.pending();
-      const currentState = await loaders.k8s.siteState({ site });
-      const desiredState = { ...currentState, owner, repo, commitSha };
-      const { deployment: { domains } } = await actions.sites.reconcile({
-        site,
-        currentState,
-        desiredState,
-        production: false,
-      });
-      statusController.suceeed(domains[0].url);
-    } catch (_err) {
-      statusController.failure();
-      throw _err;
-    }
+    await handleChange(owner, repo, commitSha, false, ctx);
   },
 };
--- admin/github/push.ts
+++ admin/github/push.ts
@@ -1,43 +1,18 @@
 import { GithubEventListener } from "../mod.ts";
-import { controllerFor, controllerGroup } from "./statusController.ts";
+import { handleChange } from "./handler.ts";
 
 export const pushEventHandler: GithubEventListener<"push"> = {
   events: ["push"],
   handle: async (event, ctx) => {
-    const { actions, loaders } = ctx.invoke["deco-sites/admin"];
     const owner = event.repository.owner.login;
     const repo = event.repository.name;
     const commitSha = event.after;
-    const site = repo; // TODO(mcandeia) it should have a way to deploy more sites to the same repo
-
-    const statusControllerGroup = controllerGroup(
-      controllerFor({
-        owner,
-        repo,
-        commitSha,
-        context: `(beta) Deco / sites-${site} / ${commitSha}`,
-      }, ctx),
-      controllerFor({
-        owner,
-        repo,
-        commitSha,
-        context: `(beta) Deco / sites-${site} / prod`,
-      }, ctx),
+    await handleChange(
+      owner,
+      repo,
+      commitSha,
+      event.ref === "refs/heads/main",
+      ctx,
     );
-    try {
-      statusControllerGroup.pending();
-      const currentState = await loaders.k8s.siteState({ site });
-      const desiredState = { ...currentState, owner, repo, commitSha };
-      const { deployment: { domains } } = await actions.sites.reconcile({
-        site,
-        currentState,
-        desiredState,
-        production: true,
-      });
-      statusControllerGroup.suceeed(domains[0].url, domains[1].url);
-    } catch (_err) {
-      statusControllerGroup.failure();
-      throw _err;
-    }
   },
 };
