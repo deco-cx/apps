935d12938cc8cc74d95bea22d11694e00962d608
Marcos Candeia <marrcooos@gmail.com>
2023-12-25 19:12:50 -0300

    Issue wildcard cert
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- platforms/kubernetes/actions/sites/create.ts
+++ platforms/kubernetes/actions/sites/create.ts
@@ -7,6 +7,32 @@ export interface Props {
   site: string;
 }
 
+const wildcardCertFor = (namespace: string) => ({
+  apiVersion: "cert-manager.io/v1",
+  kind: "Certificate",
+  metadata: {
+    name: "domain-wildcard-cert",
+    namespace,
+  },
+  spec: {
+    secretName: "domain-wildcard-cert",
+    duration: "2160h0m0s", // 90d
+    renewBefore: "360h0m0s", // 15d
+    privateKey: {
+      algorithm: "RSA",
+      encoding: "PKCS1",
+      size: 2048,
+    },
+    usages: ["server auth", "client auth"],
+    dnsNames: ["*.decocdn.com"],
+    issuerRef: {
+      name: "letsencrypt-prod-cluster",
+      kind: "ClusterIssuer",
+      group: "cert-manager.io",
+    },
+  },
+});
+
 const EFS_SC = "efs-sc";
 /**
  * Provision namespace of the new site and required resources.
@@ -18,11 +44,19 @@ export default async function newSite(
   ctx: AppContext,
 ) {
   const corev1Api = ctx.kc.makeApiClient(k8s.CoreV1Api);
+  const customObjectApi = ctx.kc.makeApiClient(k8s.CustomObjectsApi);
 
   await corev1Api.createNamespace({
     metadata: { name: site },
   }).catch(ignoreIfExists);
   await Promise.all([
+    customObjectApi.createNamespacedCustomObject(
+      "cert-manager.io",
+      "v1",
+      site,
+      "certificates",
+      wildcardCertFor(site),
+    ).catch(ignoreIfExists),
     corev1Api.createNamespacedPersistentVolumeClaim(site, {
       metadata: { name: DECO_SITES_PVC, namespace: site },
       spec: {
