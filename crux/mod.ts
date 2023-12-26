91f9b1103331b42a00caf71d8e78e422fb566dc2
guitavano <tavano62@gmail.com>
2023-12-26 12:58:36 -0300

    add logo

--- crux/mod.ts
+++ crux/mod.ts
@@ -4,10 +4,10 @@ import type { App as A } from "deco/mod.ts";
 export type App = ReturnType<typeof CRUX>;
 
 export interface State {
-    /**
-     * @title Site URL
-     * @description The URL of the site that will be used to generate the dashboard. It works for websites with high traffic volume
-     */
+  /**
+   * @title Site URL
+   * @description The URL of the site that will be used to generate the dashboard. It works for websites with high traffic volume
+   */
   siteUrl: string;
 }
 
--- crux/preview/Preview.tsx
+++ crux/preview/Preview.tsx
@@ -3,15 +3,18 @@ import { AppRuntime } from "deco/types.ts";
 import { App } from "../mod.ts";
 
 // this base URL was found at this documentation: https://developer.chrome.com/docs/crux/dashboard?hl=pt-br
-const BASE_CRUX_URL = "https://lookerstudio.google.com/embed/reporting/bbc5698d-57bb-4969-9e07-68810b9fa348/page/keDQB"
+const BASE_CRUX_URL =
+  "https://lookerstudio.google.com/embed/reporting/bbc5698d-57bb-4969-9e07-68810b9fa348/page/keDQB";
 
-export default function Preview(app: AppRuntime<BaseContext, App["state"]>){
-    const siteUrl = app.state.siteUrl
+export default function Preview(app: AppRuntime<BaseContext, App["state"]>) {
+  const siteUrl = app.state.siteUrl;
 
-    const url = BASE_CRUX_URL + `?params=%7B"origin":"${encodeURI(siteUrl)}"%7D`
-    return(
-        <div class="h-full">
-            <style dangerouslySetInnerHTML={{__html: `
+  const url = BASE_CRUX_URL + `?params=%7B"origin":"${encodeURI(siteUrl)}"%7D`;
+  return (
+    <div class="h-full">
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
             html{
                 height: 100%;
             }
@@ -19,8 +22,19 @@ export default function Preview(app: AppRuntime<BaseContext, App["state"]>){
                 height: 100%;
             }
             
-            `}}></style>
-            <iframe width="100%" height="100%" src={url} frameBorder="0" style="border:0" allowFullScreen></iframe>
-        </div>
-    )
+            `,
+        }}
+      >
+      </style>
+      <iframe
+        width="100%"
+        height="100%"
+        src={url}
+        frameBorder="0"
+        style="border:0"
+        allowFullScreen
+      >
+      </iframe>
+    </div>
+  );
 }
