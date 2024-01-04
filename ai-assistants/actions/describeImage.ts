968986fb57968ca88bc8a0a6bd9b5fb45e489dda
Itamar Rocha Filho <itamardprf@gmail.com>
2024-01-04 12:20:17 -0300

    Improve description prompt with user input

--- ai-assistants/actions/describeImage.ts
+++ ai-assistants/actions/describeImage.ts
@@ -21,9 +21,12 @@ export default async function describeImage(
             text:
               `Describe this image in few words focus on it's main characteristics. 
                             This description will be used to search similar items in an e-commerce store, 
-                            so describe name of the product and other relevant information. Use 3 words tops to describe.
+                            so describe name of the product and other relevant information. Use NO MORE than 3 words to describe the product.
                              Avoid using colors. Also, take into consideration the user prompt and describe the object it 
-                             focuses on if there is one:
+                             focuses on if there is one. Output should be 1-2 sentences and should be a request summarizing 
+                             the user's need/request to a sales assistant that will search the product in an e-commerce store.
+
+                             User prompt:
                             ${describeImageProps.userPrompt}`,
           },
           {
@@ -36,6 +39,6 @@ export default async function describeImage(
       },
     ],
   });
-  console.log(response);
+  console.log('describe image response: ', response);
   return response;
 }
