a5e95a6920747e9b50cc4e01bfc2e76e8357ce20
Itamar Rocha Filho <itamardprf@gmail.com>
2024-01-05 11:47:27 -0300

    Update manifest

--- ai-assistants/manifest.gen.ts
+++ ai-assistants/manifest.gen.ts
@@ -4,15 +4,19 @@
 
 import * as $$$0 from "./loaders/messages.ts";
 import * as $$$$$$$$$0 from "./actions/describeImage.ts";
-import * as $$$$$$$$$1 from "./actions/chat.ts";
+import * as $$$$$$$$$1 from "./actions/transcribeAudio.ts";
+import * as $$$$$$$$$2 from "./actions/awsUploadImage.ts";
+import * as $$$$$$$$$3 from "./actions/chat.ts";
 
 const manifest = {
   "loaders": {
     "ai-assistants/loaders/messages.ts": $$$0,
   },
   "actions": {
-    "ai-assistants/actions/chat.ts": $$$$$$$$$1,
+    "ai-assistants/actions/awsUploadImage.ts": $$$$$$$$$2,
+    "ai-assistants/actions/chat.ts": $$$$$$$$$3,
     "ai-assistants/actions/describeImage.ts": $$$$$$$$$0,
+    "ai-assistants/actions/transcribeAudio.ts": $$$$$$$$$1,
   },
   "name": "ai-assistants",
   "baseUrl": import.meta.url,
