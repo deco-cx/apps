399c5de816fa89200276a315630a1176ca4f3793
gimenes <tlgimenes@gmail.com>
2024-01-08 19:02:24 -0300

    wip

--- admin/actions/workspaces/connect.ts
+++ admin/actions/workspaces/connect.ts
@@ -1,31 +1,34 @@
 import { Resolvable } from "deco/engine/core/resolver.ts";
 import { genMetaInfo, MetaInfo } from "deco/routes/live/_meta.ts";
-import { debounce } from "std/async/debounce.ts";
+import { fjp } from "../../deps.ts";
 import { storage } from "../../fsStorage.ts";
-import { AppContext } from "../../mod.ts";
 
 export interface Props {
   /** Environment name to connect to */
   name: string;
 }
 
-interface UpdateBlock {
-  type: "update-block";
-  payload: { id: string; block: Resolvable };
+interface PatchState {
+  type: "patch-state";
+  payload: fjp.Operation[];
+  ack: string;
 }
 
 interface FetchState {
   type: "fetch-state";
 }
 
+interface StatePatched {
+  type: "state-patched";
+  payload: fjp.Operation[];
+  ack: string;
+  // Maybe add data and user info in here
+  metadata?: unknown;
+}
+
 interface StateFetched {
   type: "state-fetched";
   payload: State;
-}
-
-interface StatePatched {
-  type: "state-patched";
-  payload: Partial<State>;
 }
 
 interface State {
@@ -34,101 +37,106 @@ interface State {
   schema: MetaInfo["schema"];
 }
 
-type Commands = UpdateBlock | FetchState;
-type Events = StateFetched | StatePatched;
+type Commands = PatchState | FetchState;
+type Events = StatePatched | StateFetched;
 
 const subscribers: WebSocket[] = [];
 
-// Broadcast new state
-// TODO(@gimenes): Remove this debounce to fix jsx editor
-storage.onChange(debounce(async () => {
-  const { payload } = await fetchState();
-
-  const event: StatePatched = {
-    type: "state-patched",
-    payload,
-  };
-
-  for (const subscriber of subscribers) {
-    subscriber.send(JSON.stringify(event));
-  }
-}, 500));
-
-const fetchState = async (): Promise<StateFetched> => {
+const fetchState = async (): Promise<State> => {
   const [blocks, { manifest, schema }] = await Promise.all([
-    storage.state(),
+    storage.state({ forceFresh: true }),
     genMetaInfo(storage),
   ]);
 
-  return {
-    type: "state-fetched",
-    payload: { blocks, manifest, schema },
-  };
+  return { blocks, manifest, schema };
 };
 
-const action = (
-  props: Props,
-  req: Request,
-  ctx: AppContext,
-) => {
-  try {
-    const { socket, response } = Deno.upgradeWebSocket(req);
+const saveState = ({ blocks }: State): Promise<void> => storage.update(blocks);
 
-    const send = (event: Events) => {
-      socket.send(JSON.stringify(event));
-    };
+// Apply patch and save state ATOMICALLY!
+// This is easily done on play. On production, however, we probably
+// need a distributed queue
+let queue = Promise.resolve<fjp.Operation[]>([]);
+const patchState = (decofileOps: fjp.Operation[]) => {
+  queue = queue.then(async () => {
+    const state = await fetchState();
+    const observer = fjp.observe<State>(state);
 
-    const parse = (event: MessageEvent<string>): Commands =>
-      JSON.parse(event.data);
+    try {
+      await saveState(decofileOps.reduce(fjp.applyReducer, state));
 
-    const open = () => {
-      subscribers.push(socket);
-      console.log("connected", props);
-    };
+      // Wait for a while before fetching the state
+      const newState = await new Promise<State>((resolve) =>
+        setTimeout(() => resolve(fetchState()), 1e3)
+      );
 
-    const close = () => {
-      subscribers.splice(subscribers.indexOf(socket), 1);
-      console.log("closed");
-    };
+      state.manifest = newState.manifest;
+      state.schema = newState.schema;
 
-    const message = async (event: MessageEvent<string>) => {
+      return fjp.generate(observer, true);
+    } finally {
+      fjp.unobserve(state, observer);
+    }
+  });
+
+  return queue;
+};
+
+const action = (_props: Props, req: Request) => {
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  const broadcast = (event: Events) => {
+    const message = JSON.stringify(event);
+    subscribers.forEach((s) => s.send(message));
+  };
+  const send = (event: Events) => socket.send(JSON.stringify(event));
+  const parse = (event: MessageEvent<string>): Commands =>
+    JSON.parse(event.data);
+
+  const open = () => subscribers.push(socket);
+  const close = () => subscribers.splice(subscribers.indexOf(socket), 1);
+  const message = async (event: MessageEvent<string>) => {
+    const data = parse(event);
+
+    if (data.type === "patch-state") {
       try {
-        const data = parse(event);
+        const { payload: decofileOps, ack } = data;
 
-        if (data.type === "update-block") {
-          const { payload: { id, block } } = data;
-          await ctx.storage.patch({ [id]: block });
-        } else if (data.type === "fetch-state") {
-          send(await fetchState());
-        } else {
-          console.error("UNKNOWN EVENT", event);
-        }
+        const allOps = await patchState(decofileOps);
+
+        // Broadcast changes
+        broadcast({
+          type: "state-patched",
+          payload: allOps,
+          ack,
+          metadata: {}, // TODO: add metadata
+        });
       } catch (error) {
-        console.error(error);
+        console.log(error.name, error.index, error.operation);
       }
-    };
+    } else if (data.type === "fetch-state") {
+      send({ payload: await fetchState(), type: "state-fetched" });
+    } else {
+      console.error("UNKNOWN EVENT", event);
+    }
+  };
 
-    /**
-     * Handles the WebSocket connection on open event.
-     */
-    socket.onopen = open;
-    /**
-     * Handles the WebSocket connection on close event.
-     */
-    socket.onclose = close;
+  /**
+   * Handles the WebSocket connection on open event.
+   */
+  socket.onopen = open;
+  /**
+   * Handles the WebSocket connection on close event.
+   */
+  socket.onclose = close;
 
-    /**
-     * Handles the WebSocket connection on message event.
-     * @param {MessageEvent} event - The WebSocket message event.
-     */
-    socket.onmessage = message;
+  /**
+   * Handles the WebSocket connection on message event.
+   * @param {MessageEvent} event - The WebSocket message event.
+   */
+  socket.onmessage = (e) => message(e).catch(() => {});
 
-    return response;
-  } catch (error) {
-    console.error(error);
-
-    throw error;
-  }
+  return response;
 };
 
 export default action;
---
+++ admin/deps.ts
@@ -0,0 +1,1 @@
+export * as fjp from "https://esm.sh/fast-json-patch@3.1.1";
