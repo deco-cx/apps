0bf9022e92bd4234a58fdd4ceb222a7d5b5fc328
Marcos Candeia <marrcooos@gmail.com>
2024-01-12 21:14:57 -0300

    Fix admin dynamic import
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- decohub/mod.ts
+++ decohub/mod.ts
@@ -1,4 +1,5 @@
-import type { App, FnContext } from "deco/mod.ts";
+import { buildSourceMap } from "deco/blocks/utils.tsx";
+import { context, type App, type FnContext } from "deco/mod.ts";
 import { Markdown } from "./components/Markdown.tsx";
 import manifest, { Manifest } from "./manifest.gen.ts";
 
@@ -8,10 +9,35 @@ export type State = {};
 /**
  * @title Deco Hub
  */
-export default function App(
+const ADMIN_APP = "decohub/apps/admin.ts";
+export default async function App(
   state: State,
-): App<Manifest, State> {
-  return { manifest, state };
+): Promise<App<Manifest, State>> {
+  const resolvedImport = import.meta.resolve("../admin/mod.ts");
+  return {
+    manifest: {
+      ...manifest,
+      apps: {
+        ...manifest.apps,
+        ...context.play // this is an optimization to not include the admin code for everyone in case of play is not being used.
+          ? {
+            [ADMIN_APP]: await import(
+              resolvedImport
+            ),
+          }
+          : {},
+      },
+    } as Manifest,
+    state,
+    ...context.play
+      ? {
+        sourceMap: {
+          ...buildSourceMap(manifest),
+          [ADMIN_APP]: resolvedImport,
+        },
+      }
+      : {},
+  };
 }
 
 export type AppContext = FnContext<State, Manifest>;
