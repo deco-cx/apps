a7e1f1ce50f60cb442e7e4604c2a408d60cc2dd6
guitavano <tavano62@gmail.com>
2024-01-19 09:32:45 -0300

    fix canonical to lowercase (#317)
    * fix canonical to lowercase
    
    * change local of canonical
    
    * remove console.log

--- vtex/utils/legacy.ts
+++ vtex/utils/legacy.ts
@@ -127,7 +127,7 @@ export const pageTypesToSeo = (
     canonical: toCanonical(
       new URL(
         current.url
-          ? current.url.replace(/.+\.vtexcommercestable\.com\.br/, "")
+          ? current.url.replace(/.+\.vtexcommercestable\.com\.br/, "").toLowerCase()
           : url,
         url,
       ),
