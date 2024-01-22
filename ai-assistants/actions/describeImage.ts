c4ec64e11766b5032fba511683198335943d4a6b
caroluchoa <carol_uchoa8@hotmail.com>
2024-01-22 19:23:13 -0300

    deal with image error

--- ai-assistants/actions/describeImage.ts
+++ ai-assistants/actions/describeImage.ts
@@ -10,35 +10,43 @@ export interface DescribeImageProps {
 export default async function describeImage(
   describeImageProps: DescribeImageProps,
 ) {
-  const response = await openai.chat.completions.create({
-    model: "gpt-4-vision-preview",
-    messages: [
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text:
-              `Describe this image in few words focus on it's main characteristics. 
-                            This description will be used to search similar items in an e-commerce store, 
-                            so describe name of the product and other relevant information. Use NO MORE than 3 words to describe the product.
-                             Avoid using colors. Also, take into consideration the user prompt and describe the object it 
-                             focuses on if there is one. Output should be 1-2 sentences and should be a request summarizing 
-                             the user's need/request to a sales assistant that will search the product in an e-commerce store.
-
-                             User prompt:
-                            ${describeImageProps.userPrompt}`,
-          },
-          {
-            type: "image_url",
-            image_url: {
-              "url": describeImageProps.uploadURL,
+  console.log("describe image props: ", describeImageProps)
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-vision-preview",
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text:
+                `Describe this image in few words focus on it's main characteristics. 
+                              This description will be used to search similar items in an e-commerce store, 
+                              so describe name of the product and other relevant information. Use NO MORE than 3 words to describe the product.
+                               Avoid using colors. Also, take into consideration the user prompt and describe the object it 
+                               focuses on if there is one. Output should be 1-2 sentences and should be a request summarizing 
+                               the user's need/request to a sales assistant that will search the product in an e-commerce store.
+  
+                               User prompt:
+                              ${describeImageProps.userPrompt}`,
             },
-          },
-        ],
-      },
-    ],
-  });
-  console.log("describe image response: ", response);
-  return response;
+            {
+              type: "image_url",
+              image_url: {
+                "url": describeImageProps.uploadURL,
+              },
+            },
+          ],
+        },
+      ],
+    });
+    console.log("describe image response: ", response);
+    return response;
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.error.message }), {
+      status: error.status,
+      headers: error.headers,
+    });
+  }
 }
