9cd1b87609577cc33cefb10a7df8843f66df4a53
guitavano <tavano62@gmail.com>
2024-02-02 09:13:03 -0300

    manifest

--- shopify/actions/showMore/productListingPage.ts
+++ shopify/actions/showMore/productListingPage.ts
@@ -1,15 +1,21 @@
 import productListingPage from "../../loaders/ProductListingPage.ts";
 import { AppContext } from "../../mod.ts";
 
-
 export interface Props {
-    nextPage: string;
-    count: number;
+  nextPage: string;
+  count: number;
 }
 
-export default async function ShowMore(props: Props, req: Request, ctx: AppContext){
+export default async function ShowMore(
+  props: Props,
+  req: Request,
+  ctx: AppContext,
+) {
+  const response = await productListingPage(
+    { count: props.count },
+    new Request(props.nextPage || req.url, { headers: req.headers }),
+    ctx,
+  );
 
-    const response = await productListingPage({count: props.count}, new Request(props.nextPage || req.url, { headers: req.headers }), ctx)
-
-    return response
+  return response;
 }
