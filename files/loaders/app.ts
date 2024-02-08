769161fabc5478fae216d2b907ccf358f008fa2b
Marcos Candeia <marrcooos@gmail.com>
2024-02-07 21:49:37 -0300

    Set charset
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- files/loaders/app.ts
+++ files/loaders/app.ts
@@ -8,7 +8,7 @@ import website, { Props as WebSiteProps } from "../../website/mod.ts";
 
 const currdir = dirname(import.meta.url);
 const importFromString = (modData: string) =>
-  import(`data:text/tsx;base64,${btoa(encodeURIComponent(modData))}`);
+  import(`data:text/tsx;charset=utf-8;base64,${btoa(modData)}`);
 
 export interface TsContent {
   path: string;
