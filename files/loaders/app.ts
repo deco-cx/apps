72f51f8bf08d9df8b93d396e88c9c4bf167d0095
Marcos Candeia <marrcooos@gmail.com>
2024-02-09 09:03:55 -0300

    Uses import map from deco (#386)
    * Encode URI Component on build js
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Set charset
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Remove handle bars and support import map
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * use deco version from branch
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Working import from app
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add app name in front of all files
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Merge manifests
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Create files using root app
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Remove app root
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Remove consolelog
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update to deco 1.53.0
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- files/loaders/app.ts
+++ files/loaders/app.ts
@@ -1,50 +1,72 @@
-import { SourceMap } from "deco/blocks/app.ts";
+import { ImportMap } from "deco/blocks/app.ts";
 import { AppManifest } from "deco/mod.ts";
 import { dirname, join } from "std/path/mod.ts";
 import { DynamicApp } from "../../decohub/mod.ts";
-import { AppContext } from "../mod.ts";
-import { create, walk } from "../sdk.ts";
 import website, { Props as WebSiteProps } from "../../website/mod.ts";
+import { AppContext } from "../mod.ts";
+import { create, FileSystemNode, isDir, walk } from "../sdk.ts";
 
 const currdir = dirname(import.meta.url);
-const importFromString = (modData: string) =>
-  import(`data:text/tsx;base64,${btoa(modData)}`);
+export const contentToDataUri = (path: string, modData: string) =>
+  `data:text/tsx;path=${encodeURIComponent(path)};charset=utf-8;base64,${
+    btoa(modData)
+  }`;
 
 export interface TsContent {
   path: string;
   content: string;
 }
+const rewriteImports = (content: string, importMap: ImportMap): string => {
+  let initialContent = content;
+  for (const [key, value] of Object.entries(importMap.imports)) {
+    initialContent = initialContent.replace(key, value);
+  }
+  return initialContent;
+};
 const compile = async (
   blockType: keyof Omit<AppManifest, "baseUrl" | "name">,
   { path, content }: TsContent,
   manifest: AppManifest,
-  sourceMap: SourceMap,
-): Promise<[AppManifest, SourceMap]> => {
-  const tsModule = await importFromString(content).catch((err) => {
+  importMap: ImportMap,
+): Promise<AppManifest> => {
+  const dataUri = contentToDataUri(
+    join(currdir, manifest.name, path),
+    rewriteImports(content, importMap),
+  );
+  const tsModule = await import(dataUri).catch((err) => {
     console.error("could not compile module", path, err);
     return null;
   });
   if (!tsModule) {
-    return [manifest, sourceMap];
+    return manifest;
   }
-  const blockPath = join(currdir, path);
-  const blockKey = join(manifest.name, path);
 
-  return [{
+  return {
     ...manifest,
     [blockType]: {
       ...manifest[blockType],
-      [blockKey]: tsModule,
+      [path]: tsModule,
     },
-  }, {
-    ...sourceMap,
-    [blockKey]: {
-      path: blockPath,
-      content: content,
-    },
-  }];
+  };
 };
 
+const buildImportMap = (root: FileSystemNode): ImportMap => {
+  const importMap: ImportMap = { imports: {} };
+
+  for (const { path, content } of walk(root)) {
+    if (!/\.tsx?$/.test(path)) {
+      continue;
+    }
+
+    const dataUri = contentToDataUri(
+      join(currdir, path),
+      content,
+    );
+
+    importMap.imports[path] = dataUri;
+  }
+  return importMap;
+};
 export interface Props {
   name: string;
 }
@@ -56,27 +78,33 @@ const loader = async (
 ): Promise<DynamicApp> => {
   const { root = create() } = ctx;
 
+  if (!isDir(root)) {
+    throw new Error("root should be a directory");
+  }
+  const appFolder = root.nodes.find((node) => node.name === props.name);
+  if (!appFolder) {
+    throw new Error(`app ${props.name} not found`);
+  }
+  const importMap = buildImportMap(appFolder);
+
   let appManifest = {
     name: props.name,
     baseUrl: import.meta.url,
   };
-  let appSourceMap: SourceMap = {};
-
-  for (const file of walk(root)) {
+  for (const file of walk(appFolder)) {
     if (!/\.tsx?$/.test(file.path)) {
       continue;
     }
 
     const [_, blockType] = file.path.split("/");
 
-    const [newManifest, newSourceMap] = await compile(
+    const newManifest = await compile(
       blockType as keyof Omit<AppManifest, "baseUrl" | "name">,
       file,
       appManifest,
-      appSourceMap,
+      importMap,
     );
     appManifest = newManifest;
-    appSourceMap = newSourceMap;
   }
   return {
     name: props.name,
@@ -85,7 +113,7 @@ const loader = async (
         return {
           state: props,
           manifest: appManifest,
-          sourceMap: appSourceMap,
+          importMap: importMap,
           dependencies: [website(props)],
         };
       },
--- handlebars/deps.ts
+++
@@ -1,1 +0,0 @@
-export { default as HTMLRenderer } from "https://denopkg.com/deco-sites/std@1.22.12/components/HTMLRenderer.tsx";
