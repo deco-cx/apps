f66ef96517427251b7b41f26b4cecad3ae23aad8
Bruno Monteiro <brunobapmon@gmail.com>
2024-02-14 11:02:17 -0300

    feat: return isVariantOf on legacy suggestions loader

--- vtex/loaders/legacy/suggestions.ts
+++ vtex/loaders/legacy/suggestions.ts
@@ -23,7 +23,7 @@ export interface Props {
 const loaders = async (
   props: Props,
   _req: Request,
-  ctx: AppContext,
+  ctx: AppContext
 ): Promise<Suggestion | null> => {
   const { vcsDeprecated } = ctx;
   const { count = 4, query } = props;
@@ -39,7 +39,7 @@ const loaders = async (
       // Not adding suggestions to cache since queries are very spread out
       // deco: { cache: "stale-while-revalidate" },
       headers: withSegmentCookie(segment),
-    },
+    }
   ).then((res) => res.json());
 
   const searches: Suggestion["searches"] = suggestions.itemsReturned
@@ -49,7 +49,10 @@ const loaders = async (
   const products: Suggestion["products"] = suggestions.itemsReturned
     .filter(({ items }) => !!items.length)
     .map(
-      ({ items: [{ productId, itemId, imageUrl, name }], href }): Product => {
+      ({
+        items: [{ productId, itemId, imageUrl, name, nameComplete }],
+        href,
+      }): Product => {
         const url = new URL(href);
 
         return {
@@ -57,11 +60,19 @@ const loaders = async (
           productID: itemId,
           sku: itemId,
           inProductGroupWithID: productId,
+          isVariantOf: {
+            "@type": "ProductGroup",
+            name: nameComplete,
+            url: new URL(href).pathname,
+            hasVariant: [],
+            additionalProperty: [],
+            productGroupID: productId,
+          },
           image: [{ "@type": "ImageObject", url: imageUrl }],
           name,
           url: url.pathname + url.search + url.hash,
         };
-      },
+      }
     );
 
   return {
