610657c16edbeed7139e2b540403aaada3df4c99
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-02-15 17:19:03 -0400

    feat: route loader

---
+++ website/loaders/routes.ts
@@ -0,0 +1,41 @@
+import { allowCorsFor } from "deco/mod.ts";
+import { AppContext } from "../mod.ts";
+
+type Resolvable<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> = {
+  __resolveType: string;
+} & T;
+
+type HandlerValue = Resolvable<{
+  page: Resolvable<{ __resolveType: string }>;
+}>;
+
+export default function loader(
+  _props: unknown,
+  req: Request,
+  ctx: AppContext,
+) {
+  Object.entries(allowCorsFor(req)).map(([name, value]) => {
+    ctx.response.headers.set(name, value);
+  });
+
+  const routes = ctx.routes?.find((routes) =>
+    routes.some((route) =>
+      (route.handler.value as HandlerValue).__resolveType ===
+        "website/handlers/fresh.ts"
+    )
+  );
+
+  console.log(routes);
+
+  if (!routes) {
+    return [];
+  }
+
+  return routes.map((route) => ({
+    label: (route.handler.value as HandlerValue).page.__resolveType.split("-")
+      .slice(1, -1).join(" "),
+    value: route.pathTemplate,
+  }));
+}
