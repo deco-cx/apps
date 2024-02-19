4d0fbe760162708493cb873375b10a68affc99bf
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-02-19 10:36:00 -0400

    chore: manifest

--- linx-impulse/manifest.gen.ts
+++ linx-impulse/manifest.gen.ts
@@ -2,22 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from "./loaders/products/productList.ts";
-import * as $$$1 from "./loaders/products/productListingPage.ts";
-import * as $$$2 from "./loaders/search/autocomplete.ts";
-import * as $$$3 from "./loaders/search/popular.ts";
-import * as $$$4 from "./loaders/search/products.ts";
+import * as $$$0 from "./loaders/config.ts";
+import * as $$$1 from "./loaders/products/productList.ts";
+import * as $$$2 from "./loaders/products/productListingPage.ts";
+import * as $$$3 from "./loaders/search/autocomplete.ts";
+import * as $$$4 from "./loaders/search/popular.ts";
+import * as $$$5 from "./loaders/search/products.ts";
 import * as $$$$$$0 from "./sections/Analytics/LinxImpulsePageView.tsx";
 import * as $$$$$$1 from "./sections/Script/LinxImpulseScript.tsx";
 import * as $$$$$$$$$0 from "./actions/analytics/sendEvent.ts";
 
 const manifest = {
   "loaders": {
-    "linx-impulse/loaders/products/productList.ts": $$$0,
-    "linx-impulse/loaders/products/productListingPage.ts": $$$1,
-    "linx-impulse/loaders/search/autocomplete.ts": $$$2,
-    "linx-impulse/loaders/search/popular.ts": $$$3,
-    "linx-impulse/loaders/search/products.ts": $$$4,
+    "linx-impulse/loaders/config.ts": $$$0,
+    "linx-impulse/loaders/products/productList.ts": $$$1,
+    "linx-impulse/loaders/products/productListingPage.ts": $$$2,
+    "linx-impulse/loaders/search/autocomplete.ts": $$$3,
+    "linx-impulse/loaders/search/popular.ts": $$$4,
+    "linx-impulse/loaders/search/products.ts": $$$5,
   },
   "sections": {
     "linx-impulse/sections/Analytics/LinxImpulsePageView.tsx": $$$$$$0,
