c75a1c94635ce36e7cf1cda9cc50acb0f9d228f0
Hugo Cabral <hugocavalcanticabral@gmail.com>
2024-03-20 14:55:58 -0300

    adds base versions for use precompile

--- platforms/kubernetes/common/cmds/build.ts
+++ platforms/kubernetes/common/cmds/build.ts
@@ -1,4 +1,5 @@
 import { DenoJSON } from "../../types.ts";
+import { compareVersions } from "https://deno.land/x/compare_versions@0.4.0/mod.ts";
 
 async function build() {
   const { ensureFile } = await import(
@@ -254,11 +255,16 @@ async function build() {
       hasChange = true;
     }
 
-    if (denoJson.compilerOptions.jsx === "react-jsx") {
-      denoJson.compilerOptions.jsx = "precompile";
-      denoJson.compilerOptions.jsxImportSource = "preact";
+    const freshVersion = denoJson.imports?.["$fresh/"];
+    const preactVersion = denoJson.imports?.["preact"];
 
-      hasChange = true;
+    if (denoJson.compilerOptions.jsx === "react-jsx" && freshVersion && preactVersion) {
+      if (compareVersions(preactVersion, '10.19.1') >= 0 && compareVersions(freshVersion, '1.6.0') >= 0) {
+        denoJson.compilerOptions.jsx = "precompile";
+        denoJson.compilerOptions.jsxImportSource = "preact";
+  
+        hasChange = true;
+      }
     }
 
     return { denoJson, hasChange };
