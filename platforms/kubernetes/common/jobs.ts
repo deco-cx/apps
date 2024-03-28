836a58416b6f3a0cb51f21b1f1a0fa090f7597c9
Marcos Candeia <marrcooos@gmail.com>
2024-03-28 17:00:08 -0300

    Wait route to be ready (#492)
    * Add watcher for route to be ready
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use build cache from start
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- platforms/kubernetes/common/jobs.ts
+++ platforms/kubernetes/common/jobs.ts
@@ -1,52 +1,29 @@
-import { delay } from "std/async/mod.ts";
 import { k8s } from "../deps.ts";
 
-export async function watchJobStatus(
+import { watchCondition } from "./watcher.ts";
+
+export function watchJobStatus(
   kc: k8s.KubeConfig,
   namespace: string,
   jobName: string,
   timeoutMs?: number,
 ): Promise<k8s.V1Job | null> {
-  const fieldSelector = `metadata.name=${jobName}`;
-  const watcher = new k8s.Watch(kc);
-
-  const { resolve, reject, promise } = Promise.withResolvers<
-    k8s.V1Job | null
-  >();
-  let lastSeenJob: k8s.V1Job | null = null;
-
-  // Watch for changes to the Job status
-  const req = await watcher.watch(
-    `/apis/batch/v1/namespaces/${namespace}/jobs`,
-    { fieldSelector },
-    (_type, obj) => {
-      lastSeenJob = obj;
+  return watchCondition(kc, jobName, {
+    path: `/apis/batch/v1/namespaces/${namespace}/jobs`,
+    conditionMet: (_type, obj) => {
       if (obj.status && obj.status.conditions) {
         const conditions = obj.status.conditions;
-        const condition: k8s.V1JobCondition = (conditions ?? []).find((
-          cond: k8s.V1JobCondition,
-        ) => cond.status === "True");
+        const condition: k8s.V1JobCondition | undefined = (conditions ?? [])
+          .find((
+            cond: k8s.V1JobCondition,
+          ) => cond.status === "True");
 
         if (condition !== undefined) {
-          resolve(lastSeenJob);
+          return true;
         }
       }
+      return false;
     },
-    (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(lastSeenJob);
-      }
-    },
-  );
-
-  if (timeoutMs) {
-    delay(timeoutMs).then(() => {
-      req.abort();
-      reject({ message: "watch timed out" });
-    });
-  }
-
-  return promise;
+    timeoutMs,
+  });
 }
