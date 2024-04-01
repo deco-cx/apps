6bb1178c34e0890f6943da13465d37dc143231d7
Tiago Gimenes <tlgimenes@gmail.com>
2024-04-01 00:15:07 -0300

    feat: HTMX (#497)
    * add htmx
    
    * add to decohub
    
    * add ghtmx

---
+++ htmx/mod.ts
@@ -0,0 +1,18 @@
+import { App, FnContext } from "deco/mod.ts";
+import manifest, { Manifest } from "./manifest.gen.ts";
+
+export type AppContext = FnContext<Props, Manifest>;
+
+export interface Props {
+  /** @default 1.9.11 */
+  version?: string;
+
+  /** @defaul https://cdn.jsdelivr.net/npm  */
+  cdn?: string;
+}
+
+export default function Site(state: Props): App<Manifest, Props> {
+  return { state, manifest };
+}
+
+export { onBeforeResolveProps } from "../website/mod.ts";
