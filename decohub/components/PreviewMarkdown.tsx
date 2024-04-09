e855021cec17b9673011a0150b66b14bb001df2a
Luis Sousa <lv-ss@hotmail.com>
2024-04-09 16:01:04 -0300

    feat: new Preview

---
+++ decohub/components/PreviewMarkdown.tsx
@@ -0,0 +1,26 @@
+import { render } from "https://deno.land/x/gfm@0.3.0/mod.ts";
+import "https://esm.sh/prismjs@1.27.0/components/prism-diff.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
+
+export const Markdown2 = async (path: string) => {
+  const content = await fetch(path).then((res) => res.text());
+
+  return () => {
+    return (
+      <>
+        <div
+          class="w-full !max-w-full px-4 prose !prose-invert"
+          dangerouslySetInnerHTML={{
+            __html: render(content, {
+              allowIframes: true,
+              allowMath: true,
+              disableHtmlSanitization: true,
+            }),
+          }}
+        />
+      </>
+    );
+  };
+};
