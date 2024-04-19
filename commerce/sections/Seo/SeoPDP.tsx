2caeca34ddc1ac11570e383df6936b6b6f7b71d1
Tiago Castro <tiagocastro602@gmail.com>
2024-04-19 14:43:29 -0300

    fix: consider the 'noIndexing' property when disabling page indexing

--- commerce/sections/Seo/SeoPDP.tsx
+++ commerce/sections/Seo/SeoPDP.tsx
@@ -1,11 +1,11 @@
-import Seo, { Props as SeoProps } from "../../../website/components/Seo.tsx";
-import { ProductDetailsPage } from "../../types.ts";
-import { canonicalFromBreadcrumblist } from "../../utils/canonical.ts";
+import Seo, { Props as SeoProps } from "../../../website/components/Seo.tsx"
+import { ProductDetailsPage } from "../../types.ts"
+import { canonicalFromBreadcrumblist } from "../../utils/canonical.ts"
 
 export type Props = {
-  jsonLD: ProductDetailsPage | null;
-  omitVariants?: boolean;
-} & Partial<Omit<SeoProps, "jsonLDs">>;
+  jsonLD: ProductDetailsPage | null
+  omitVariants?: boolean
+} & Partial<Omit<SeoProps, "jsonLDs">>
 
 /**
  * @deprecated true
@@ -13,18 +13,18 @@ export type Props = {
  * @title SeoPDP deprecated
  */
 function Section({ jsonLD, omitVariants, ...props }: Props) {
-  const title = jsonLD?.seo?.title;
-  const description = jsonLD?.seo?.description;
-  const image = jsonLD?.product.image?.[0]?.url;
+  const title = jsonLD?.seo?.title
+  const description = jsonLD?.seo?.description
+  const image = jsonLD?.product.image?.[0]?.url
   const canonical = jsonLD?.seo?.canonical
     ? jsonLD.seo.canonical
     : jsonLD?.breadcrumbList
     ? canonicalFromBreadcrumblist(jsonLD?.breadcrumbList)
-    : undefined;
-  const noIndexing = !jsonLD || jsonLD.seo?.noIndexing;
+    : undefined
+  const noIndexing = props.noIndexing || !jsonLD || jsonLD.seo?.noIndexing
 
   if (omitVariants && jsonLD?.product.isVariantOf?.hasVariant) {
-    jsonLD.product.isVariantOf.hasVariant = [];
+    jsonLD.product.isVariantOf.hasVariant = []
   }
 
   return (
@@ -37,7 +37,7 @@ function Section({ jsonLD, omitVariants, ...props }: Props) {
       jsonLDs={[jsonLD]}
       noIndexing={noIndexing}
     />
-  );
+  )
 }
 
-export default Section;
+export default Section
--- website/sections/Seo/SeoV2.tsx
+++ website/sections/Seo/SeoV2.tsx
@@ -1,38 +1,40 @@
-import Seo, { Props as SeoProps } from "../../components/Seo.tsx";
-import { renderTemplateString, SEOSection } from "../../components/Seo.tsx";
-import { AppContext } from "../../mod.ts";
+import Seo, { Props as SeoProps } from "../../components/Seo.tsx"
+import { renderTemplateString, SEOSection } from "../../components/Seo.tsx"
+import { AppContext } from "../../mod.ts"
 
 type Props = Pick<
   SeoProps,
-  "title" | "description" | "type" | "favicon" | "image" | "themeColor"
->;
+  | "title"
+  | "description"
+  | "type"
+  | "favicon"
+  | "image"
+  | "themeColor"
+  | "noIndexing"
+>
 
-export function loader(
-  props: Props,
-  _req: Request,
-  ctx: AppContext,
-) {
+export function loader(props: Props, _req: Request, ctx: AppContext) {
   const {
     titleTemplate = "",
     descriptionTemplate = "",
     title: appTitle = "",
     description: appDescription = "",
     ...seoSiteProps
-  } = ctx.seo ?? {};
-  const { title: _title, description: _description, ...seoProps } = props;
-  const title = renderTemplateString(titleTemplate, _title ?? appTitle);
+  } = ctx.seo ?? {}
+  const { title: _title, description: _description, ...seoProps } = props
+  const title = renderTemplateString(titleTemplate, _title ?? appTitle)
   const description = renderTemplateString(
     descriptionTemplate,
-    _description ?? appDescription,
-  );
+    _description ?? appDescription
+  )
 
-  return { ...seoSiteProps, ...seoProps, title, description };
+  return { ...seoSiteProps, ...seoProps, title, description }
 }
 
 function Section(props: Props): SEOSection {
-  return <Seo {...props} />;
+  return <Seo {...props} />
 }
 
-export { default as Preview } from "../../components/_seo/Preview.tsx";
+export { default as Preview } from "../../components/_seo/Preview.tsx"
 
-export default Section;
+export default Section
