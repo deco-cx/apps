ff07739c9b61c858a7e32cfbce02dffb57dbbb11
vitortrindader <vitortrriberio@gmail.com>
2024-04-23 17:14:46 -0300

    adicionando logica de mudar o min de scalling para pods sem serem em prod

--- platforms/kubernetes/actions/deployments/rollout.ts
+++ platforms/kubernetes/actions/deployments/rollout.ts
@@ -2,6 +2,7 @@ import { revisionRoute } from "../../common/knative/route.ts";
 import { upsertObject } from "../../common/objects.ts";
 import { AppContext } from "../../mod.ts";
 import { Namespace } from "../sites/create.ts";
+import { k8s } from "../../deps.ts";
 
 export interface Props {
   site: string;
@@ -21,8 +22,24 @@ export const Routes = {
 export default async function rollout(
   { site, deploymentId }: Props,
   _req: Request,
-  ctx: AppContext,
+  ctx: AppContext
 ) {
+  const k8sApi = ctx.kc.makeApiClient(k8s.CustomObjectsApi);
+
+  const currentProdRevision = await k8sApi
+    .getNamespacedCustomObject(
+      "serving.knative.dev",
+      "v1",
+      Namespace.forSite(site),
+      "routes",
+      Routes.prod(site)
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const oldRevision = currentProdRevision.body.spec.traffic[0].revisionName;
+
   const revisionName = `${site}-site-${deploymentId}`;
   await upsertObject(
     ctx.kc,
@@ -33,6 +50,31 @@ export default async function rollout(
     }),
     "serving.knative.dev",
     "v1",
-    "routes",
+    "routes"
+  );
+
+  const oldProdRevision = await k8sApi
+    .getNamespacedCustomObject(
+      "serving.knative.dev",
+      "v1",
+      Namespace.forSite(site),
+      "revisions",
+      oldRevision
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const oldProdRevisionBody = oldProdRevision.body;
+  oldProdRevisionBody.metadata.annotations[
+    "autoscaling.knative.dev/min-scale"
+  ] = "0";
+
+  await upsertObject(
+    ctx.kc,
+    oldProdRevisionBody,
+    "serving.knative.dev",
+    "v1",
+    "revisions"
   );
 }
