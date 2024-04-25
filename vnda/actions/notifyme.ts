3b1cba70dd7c2fe4b5858e6ed77a0f01411a1bc4
gabrielMatosBoubee <gm.boubee@gmail.com>
2024-04-25 16:02:41 -0300

    feat: finish warn-me

--- vnda/actions/notifyme.ts
+++ vnda/actions/notifyme.ts
@@ -3,40 +3,35 @@ import { AppContext } from "../mod.ts";
 export interface Props {
   key: string;
   sku: string;
-  nome: string;
-  url: string;
-  referencia: string;
   email: string;
 }
 
 const action = async (
-    _props: unknown,
-    _req: Request,
-    ctx: AppContext,
-  ):Promise<void> => {
-    const { authToken } = ctx
-    const stringAuthToken = typeof authToken === "string"
-    ? authToken
-    : authToken?.get?.() ?? "";
+  props: Props,
+  _req: Request,
+  ctx: AppContext,
+): Promise<void> => {
+  const { account } = ctx;
+  const { key, sku, email } = props;
 
-    const options = {
-      method: 'POST',
-      headers: new Headers({
-        "User-Agent": "decocx/1.0",
-        "X-Shop-Host": "https://velocita.cdn.vnda.com.br/lista_de_espera",
-        "accept": "application/json",
-        authorization: `Bearer ${stringAuthToken}`,
-      }),
-      body: JSON.stringify({
-        key: "velocita-avise-me",
-        sku: "DD6831-392-P",
-        nome: "Shorts Run Nike Swoosh Run Feminino",
-        url: "/produto/shorts-run-nike-swoosh-run-feminino-4169",
-        referencia: "DD6831-392",
-        email: "gustavo.dahm@vnda.com.br",
-      })
-    };
-    const response = await fetch(`https://velocita.cdn.vnda.com.br/lista_de_espera`, options)
+  const formdata = new FormData();
+  formdata.append("key", key);
+  formdata.append("sku", sku);
+  formdata.append("email", email);
+
+  const options = {
+    method: "POST",
+    body: formdata,
+  };
+
+  try {
+    await fetch(
+      `https://${account}.cdn.vnda.com.br/lista_de_espera`,
+      options,
+    ).then((res) => res.json());
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  export default action;
+export default action;
