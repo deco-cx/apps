4ff98d5afb1f02f4b407d4d07811cc280d6a9883
Marcos Candeia <marrcooos@gmail.com>
2024-04-26 09:30:42 -0300

    Make preview deployments be virutalized
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- platforms/kubernetes/common/cmds/hypervisor.ts
+++ platforms/kubernetes/common/cmds/hypervisor.ts
@@ -1,9 +1,8 @@
 import { baseRun, runCmd } from "./cmd.ts";
 
-const HYPERVISOR_VERSION = "bd66446d24f69ff82a98cc92364adae7adba6103";
 const script = `
 ${baseRun}
-deno run --node-modules-dir=false --import-map https://denopkg.com/deco-sites/play@${HYPERVISOR_VERSION}/deno.json -Ar https://denopkg.com/deco-sites/play@${HYPERVISOR_VERSION}/main.ts --build-cmd "deno task build" -- ${
+deno run -A --import-map=$(deno eval 'console.log(import.meta.resolve("deco/deno.json"))') $(deno eval 'console.log(import.meta.resolve("deco/hypervisor/main.ts"))')  --build-cmd "deno task build" -- ${
   runCmd(true)
 }
 `;
