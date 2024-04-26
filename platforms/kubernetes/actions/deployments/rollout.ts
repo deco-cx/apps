d65f2009d37db7aed04f9f1bc148a8c43c13e5b8
vitortrindader <vitortrriberio@gmail.com>
2024-04-26 09:42:17 -0300

    Criando arquivo utils com funções utilizadas

--- platforms/kubernetes/actions/deployments/rollout.ts
+++ platforms/kubernetes/actions/deployments/rollout.ts
@@ -3,93 +3,36 @@ import { upsertObject } from "../../common/objects.ts";
 import { AppContext } from "../../mod.ts";
 import { Namespace } from "../sites/create.ts";
 import { k8s } from "../../deps.ts";
-import { logger } from "deco/observability/otel/config.ts";
+
 import {
-  PLURAL_REVISIONS,
   PLURAL_ROUTES,
   GROUP_SERVINGKNATIVEDEV,
   VERSION_V1,
 } from "../../constants.ts";
+import {
+  allowScaleToZero,
+  getProdRevision,
+} from "../../common/knative/utils.ts";
 
 export interface Props {
   site: string;
   deploymentId: string;
 }
 
-export interface MinPodsProps {
-  k8sApi: k8s.CustomObjectsApi;
-  site: string;
-  oldRevision: string;
-}
-
-export interface UpdateRevision {
-  ctx: AppContext;
-  site: string;
-  oldRevision: string;
+interface CurrentProdRevisionBody {
+  spec: {
+    traffic: {
+      latestRevision: boolean;
+      percent: number;
+      revisionName: string;
+    }[];
+  };
 }
 
 export const Routes = {
   prod: (site: string) => `sites-${site}`,
   preview: (site: string, deploymentId: string) =>
     `${Routes.prod(site)}-${deploymentId}`,
-};
-
-const getCurrentProdRevision = async ({
-  k8sApi,
-  site,
-}: {
-  k8sApi: k8s.CustomObjectsApi;
-  site: string;
-}) => {
-  const currentProdRevision = await k8sApi
-    .getNamespacedCustomObject(
-      GROUP_SERVINGKNATIVEDEV,
-      VERSION_V1,
-      Namespace.forSite(site),
-      PLURAL_ROUTES,
-      Routes.prod(site)
-    )
-    .catch((err) => {
-      logger.error(err);
-    });
-  return currentProdRevision;
-};
-
-const updtadeOldRevision = async ({
-  oldRevision,
-  site,
-  ctx,
-}: UpdateRevision) => {
-  const oldProdRevisionBody = {
-    metadata: {
-      name: oldRevision,
-      namespace: Namespace.forSite(site),
-    },
-  };
-
-  if (!oldProdRevisionBody) {
-    return;
-  }
-
-  await upsertObject(
-    ctx.kc,
-    oldProdRevisionBody,
-    GROUP_SERVINGKNATIVEDEV,
-    VERSION_V1,
-    PLURAL_REVISIONS,
-    (current) => {
-      return {
-        ...current,
-        metadata: {
-          ...current.metadata,
-          annotations: {
-            ...current.metadata.annotations,
-            "autoscaling.knative.dev/min-scale": "0",
-          },
-        },
-      };
-    }
-  );
 };
 
 /**
@@ -102,8 +45,11 @@ export default async function rollout(
   ctx: AppContext
 ) {
   const k8sApi = ctx.kc.makeApiClient(k8s.CustomObjectsApi);
-  const currentProdRevision = await getCurrentProdRevision({ k8sApi, site });
-  const oldRevision = currentProdRevision?.body.spec.traffic[0].revisionName;
+  const currentProdRevision = await getProdRevision({ k8sApi, site });
+  const currentProdRevisionBody =
+    currentProdRevision?.body as CurrentProdRevisionBody;
+  const previousProdRevisionName =
+    currentProdRevisionBody?.spec.traffic[0].revisionName;
 
   const revisionName = `${site}-site-${deploymentId}`;
   await upsertObject(
@@ -118,5 +64,5 @@ export default async function rollout(
     PLURAL_ROUTES
   );
 
-  await updtadeOldRevision({ oldRevision, site, ctx });
+  allowScaleToZero({ revision: previousProdRevisionName, site, ctx });
 }
