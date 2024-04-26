d149d9dac660acb1d8e5fedf213e0183efa194c6
Marcos Candeia <marrcooos@gmail.com>
2024-04-26 13:44:37 -0300

    Do not fail on old deco versions
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- platforms/kubernetes/common/cmds/hypervisor.ts
+++ platforms/kubernetes/common/cmds/hypervisor.ts
@@ -2,9 +2,25 @@ import { baseRun, runCmd } from "./cmd.ts";
 
 const script = `
 ${baseRun}
-deno run -A --import-map=$(deno eval 'console.log(import.meta.resolve("deco/deno.json"))') $(deno eval 'console.log(import.meta.resolve("deco/hypervisor/main.ts"))')  --build-cmd "deno task build" -- ${
+if [[ -f "./deno.jsonc" ]]; then
+    LOAD_CONFIG_ARG="--config=deno.json"
+else
+  if [ -f "./import_map.json" ]; then
+    LOAD_CONFIG_ARG="--import-map=import_map.json"
+  else
+    LOAD_CONFIG_ARG="--config=deno.json"
+  fi
+fi
+HYPERVISOR_MAIN=$(deno eval 'console.log(import.meta.resolve("deco/hypervisor/main.ts"))')
+deno info $LOAD_CONFIG_ARG $HYPERVISOR_MAIN &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+  ${runCmd()}
+else
+  deno run -A $LOAD_CONFIG_ARG $HYPERVISOR_MAIN --build-cmd "deno task build" -- ${
   runCmd(true)
 }
+fi
 `;
 
 export default script;
