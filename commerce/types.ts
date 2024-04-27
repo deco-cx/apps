08e1245f7f398a9c6a0a90e6e42e1c1078b31a17
LeHenriqueAlmeida <henriquexavier@lelabs.com.br>
2024-04-27 08:46:17 -0300

    The type is an optional property named advertisement to the code. This property is a string and is used to provide information about VTEX Ads, in search results and categories. This allows developers to customize their marketing strategies according to the specific needs and goals of the marketing team.

--- commerce/types.ts
+++ commerce/types.ts
@@ -487,6 +487,8 @@ export interface Product extends Omit<Thing, "@type"> {
   sku: string;
   /** A pointer to another product (or multiple products) for which this product is an accessory or spare part. */
   isAccessoryOrSparePartFor?: ProductLeaf[];
+  // The type is an optional property named "advertisement" to the code. This property is a string and is used to provide information about VTEX Ads, in search results and categories. This allows developers to customize their marketing strategies according to the specific needs and goals of the marketing team.
+  advertisement?: string;
 
   questions?: Question[];
 }
