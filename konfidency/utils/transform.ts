0c12b59a62f7fac8fba548f8aae1981ed55de410
gabrielMatosBoubee <gm.boubee@gmail.com>
2024-05-02 15:50:12 -0300

    fix: structured data

--- konfidency/utils/transform.ts
+++ konfidency/utils/transform.ts
@@ -4,24 +4,6 @@ import type { AggregateRating, Review } from "../../commerce/types.ts";
 const MAX_RATING_VALUE = 5;
 const MIN_RATING_VALUE = 0;
 
-const getBestAndWorstReview = (array: PDPReview["reviews"][0]["reviews"]) => {
-  if (!array || array.length === 0) {
-    return { bestRating: MIN_RATING_VALUE, worstRating: MIN_RATING_VALUE };
-  }
-
-  const { bestRating, worstRating } = array.reduce((acc, curr) => {
-    if (curr.rating > acc.bestRating) {
-      acc.bestRating = curr.rating;
-    }
-    if (curr.rating < acc.worstRating) {
-      acc.worstRating = curr.rating;
-    }
-    return acc;
-  }, { bestRating: MIN_RATING_VALUE, worstRating: MAX_RATING_VALUE });
-
-  return { bestRating, worstRating };
-};
-
 export const toReview = (
   ProductReviews: PDPReview["reviews"][0],
 ): { aggregateRating: AggregateRating; review: Review[] } => {
@@ -42,18 +24,14 @@ export const toReview = (
     }) as Review
   );
 
-  const { bestRating, worstRating } = getBestAndWorstReview(
-    ProductReviews.reviews,
-  );
-
   return {
     review,
     aggregateRating: {
       "@type": "AggregateRating",
       reviewCount: ProductReviews.reviewCount,
       ratingValue: ProductReviews.aggregateRating,
-      bestRating,
-      worstRating,
+      bestRating: MAX_RATING_VALUE,
+      worstRating: MIN_RATING_VALUE,
     },
   };
 };
