88ad55f03ed1c5e89a079b5665fb03d1059ba514
Jonas Jesus <jonasjesus@carcara.tech>
2024-05-07 13:08:53 -0300

    fix: removing error handling and improving return

--- magento/actions/cart/simulation.ts
+++ magento/actions/cart/simulation.ts
@@ -2,7 +2,6 @@ import { AppContext } from "../../mod.ts";
 import type {
   ShippingMethod,
 } from "../../utils/client/types.ts";
-import { badRequest } from "deco/mod.ts";
 
 export interface Props {
   cartId: string;
@@ -18,24 +17,19 @@ const action = async (
   const { clientAdmin, site } = ctx;
   const { cartId, countryId, postcode } = props;
 
-  if (!cartId || !countryId || !postcode) {
-    badRequest({
-      message: "could not find some props",
-    });
-  }
   const shippingMethod = await clientAdmin
-    ["POST /rest/:site/V1/carts/:cartId/estimate-shipping-methods"]({
-      site,
-      cartId,
-    }, {
-      body: {
-        address: {
-          postcode,
-          countryId,
-        }
-      },
-    });
-  return shippingMethod.json();
+  ["POST /rest/:site/V1/carts/:cartId/estimate-shipping-methods"]({
+    site,
+    cartId,
+  }, {
+    body: {
+      address: {
+        postcode,
+        countryId,
+      }
+    },
+  }).then((res) => res.json());
+  return shippingMethod
 };
 
 export default action;
