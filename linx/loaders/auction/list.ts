2039697ee94ed624d6274d2daeb891fb46a1f903
ana-agenciafg <99687305+ana-agenciafg@users.noreply.github.com>
2024-05-07 16:48:37 -0300

    Update facets auction - list.ts (#577)

--- linx/loaders/auction/list.ts
+++ linx/loaders/auction/list.ts
@@ -1,7 +1,7 @@
 import type { AppContext } from "../../../linx/mod.ts";
 import { nullOnNotFound } from "../../../utils/http.ts";
 import { isAuctionModel } from "../../utils/paths.ts";
-import { toAuction } from "../../utils/transform.ts";
+import {  toFilters, toAuction } from "../../utils/transform.ts";
 import { AuctionListingPage } from "../../utils/types/auction.ts";
 
 /**
@@ -35,7 +35,7 @@ const loader = async (
   const products = auctions.Model.ProductAuctions.map((auction) =>
     toAuction(auction, { cdn })
   );
-  const facets = auctions.Model.Grid.Facets;
+  const facets = toFilters(auctions.Model.Grid.Facets,url);
   const pageCount = auctions.Model.Grid.PageCount;
   const pageNumber = auctions.Model.Grid.PageNumber;
   const pageIndex = auctions.Model.Grid.PageIndex;
