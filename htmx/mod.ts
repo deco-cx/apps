6e1769c0e87ab793634ef53db9bf7bb4beed80cd
gimenes <tlgimenes@gmail.com>
2024-05-08 14:31:09 -0300

    fix htmx config

--- htmx/mod.ts
+++ htmx/mod.ts
@@ -3,12 +3,39 @@ import manifest, { Manifest } from "./manifest.gen.ts";
 
 export type AppContext = FnContext<Props, Manifest>;
 
+export type Extension =
+  | "ajax-header"
+  | "alpine-morph"
+  | "class-tools"
+  | "client-side-templates"
+  | "debug"
+  | "event-header"
+  | "head-support"
+  | "include-vals"
+  | "json-enc"
+  | "idiomorph"
+  | "loading-states"
+  | "method-override"
+  | "morphdom-swap"
+  | "multi-swap"
+  | "path-deps"
+  | "preload"
+  | "remove-me"
+  | "response-targets"
+  | "restored"
+  | "server-sent-events"
+  | "web-sockets"
+  | "path-params";
+
 export interface Props {
   /** @default 1.9.11 */
   version?: string;
 
   /** @defaul https://cdn.jsdelivr.net/npm  */
   cdn?: string;
+
+  /** @title HTMX extensions to include */
+  extensions?: Extension[];
 }
 
 /**
@@ -17,6 +44,13 @@ export interface Props {
  * @category Frameworks
  * @logo https://raw.githubusercontent.com/deco-cx/apps/main/htmx/logo.png
  */
-export default function Site(state: Props): App<Manifest, Props> {
-  return { state, manifest };
+export default function Site(state: Props): App<Manifest, Required<Props>> {
+  return {
+    state: {
+      version: state.version ?? "1.9.11",
+      cdn: state.cdn ?? "https://cdn.jsdelivr.net/npm",
+      extensions: state.extensions ?? [],
+    },
+    manifest,
+  };
 }
