f08276b4589961697129e1729c95e38233ac6091
Marcos Candeia <marrcooos@gmail.com>
2024-05-08 12:17:35 -0300

    Create branch only route
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- admin/github/handler.ts
+++ admin/github/handler.ts
@@ -17,26 +17,25 @@ export const handleChange = async (
   const { loaders } = ctx.invoke["deco-sites/admin"];
   const reqUrl = new URL(req.url);
   const site = reqUrl.searchParams.get("site") ?? repo;
-  const statusControllerGroup = controllerGroup(
-    controllerFor({
-      owner,
-      repo,
-      commitSha,
-      context: `Deco / site-${site} / commit`,
-    }, ctx),
-    production
-      ? controllerFor({
+  const controllers = production
+    ? [
+      controllerFor({
         owner,
         repo,
         commitSha,
         context: `Deco / site-${site} / prod`,
-      }, ctx)
-      : controllerFor({
+      }, ctx),
+    ]
+    : [
+      controllerFor({
         owner,
         repo,
         commitSha,
         context: `Deco / site-${site} / ${refSlug}`,
       }, ctx),
+    ];
+  const statusControllerGroup = controllerGroup(
+    ...controllers,
   );
   try {
     const platform = await loaders.platforms.forSite({ site }).then((p) => p)
