0aa8d6eb949c44e5ce5de607cbe390e8260e1dc6
Jonathan Lopes <jonathanwlopes93@gmail.com>
2024-05-14 12:09:32 -0300

    Update website/loaders/redirects.ts
    Co-authored-by: Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>

--- website/loaders/redirects.ts
+++ website/loaders/redirects.ts
@@ -20,7 +20,7 @@ async function getAllRedirects(ctx: AppContext): Promise<Route[]> {
         __resolveType: "website/handlers/redirect.ts",
         to: redirect.to,
         type: redirect.type,
-        keepQueryParameters: redirect.keepQueryParameters,
+        discardQueryParameters: redirect.discardQueryParameters,
       },
     },
   }));
