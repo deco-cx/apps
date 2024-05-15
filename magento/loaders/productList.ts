7159f1c128a6bdc2a4789d7b07b0a06fb75c823f
Lucas Sacci <aka-sacci@MacBook-Air-de-Lucas.local>
2024-05-14 22:13:14 -0300

    types extended

--- magento/loaders/productList.ts
+++ magento/loaders/productList.ts
@@ -2,20 +2,20 @@ import type { ProductDetailsPage } from "../../commerce/types.ts";
 /* import type { RequestURLParam } from "../../website/functions/requestToParam.ts"; */
 import { AppContext } from "../mod.ts";
 import {
-  GraphQLShelf,
-  GraphQLShelfQuery,
+  GraphQLProductShelf,
+  GraphQLProductShelfInputs,
 } from "../utils/clientGraphql/types.ts";
 import { gql } from "../../utils/graphql.ts";
 /* import { URL_KEY } from "../utils/constants.ts";
 import stringifySearchCriteria from "../utils/stringifySearchCriteria.ts";
 import { toBreadcrumbList, toProduct, toSeo } from "../utils/transform.ts"; */
 
-//export interface Props {}
+/* ßß */
 
 export const GetProduct = { 
   fragments: [],
   query: gql`{
-    products(search: "Sabonete", pageSize: 2) {
+    products(search: "Sabonete", pageSize: 1) {
       total_count
       items {
         name
@@ -32,6 +32,13 @@ export const GetProduct = {
         page_size
         current_page
       }
+      sort_fields {
+        default
+        options {
+          label
+          value
+        }
+      }
     }
   }`,
 };
@@ -46,11 +53,11 @@ async function loader(
 ): Promise<ProductDetailsPage | null> {
   const { clientGraphql } = ctx;
   try {
-    const data = await clientGraphql.query<GraphQLShelf, GraphQLShelfQuery>({
+    const data = await clientGraphql.query<GraphQLProductShelf, GraphQLProductShelfInputs>({
         variables: {},
         ...GetProduct,
       });
-    console.log(data)
+    console.log(data.products.items)
   } catch (error) {
     console.log(error)
   }
--- magento/utils/clientGraphql/types.ts
+++ magento/utils/clientGraphql/types.ts
@@ -1,3 +1,4 @@
+//Leaf Elements
 export interface GraphQLProductLeaf {
   canonical_url: string;
   country_of_manufacture: string;
@@ -22,10 +23,115 @@ export interface GraphQLProductLeaf {
   url_suffix?: string;
 }
 
-export type GraphQLShelf = GraphQLProductLeaf;
+export interface GraphQLProductQueryLeaf {
+  total_count?: number;
+  items?: GraphQLProductLeaf[];
+  //TODO: AGGREGGATION FILTER INPUT
+  aggreggations?: Aggreggation[];
+  page_info?: SearchResultPageInfo;
+  sort_fields?: SortFields;
+  suggestions?: SearchSuggestion[];
+}
 
-export interface GraphQLShelfQuery {
+export interface Aggreggation {
+  attribute_code?: string;
+  count?: number;
+  label?: string;
+  position?: number;
+  options?: AggreggationOption[];
+}
+
+export interface AggreggationOption {
+  count?: number;
+  label?: string;
+  value?: string;
+}
+
+export interface SearchResultPageInfo {
+  current_page?: number;
+  page_size?: number;
+  total_pages?: number;
+}
+
+export interface SortFields {
+  default?: string;
+  options?: SortField[];
+}
+
+export interface SortField {
+  label?: string;
+  value?: string;
+}
+
+export interface SearchSuggestion {
+  search?: string;
+}
+
+//Querys and Returns
+export interface GraphQLProductShelf {
+  products: GraphQLProductQueryLeaf;
+}
+
+export interface GraphQLProductShelfInputs {
   search?: string;
   pageSize?: number;
   currentPage?: number;
+  filter?: GraphQLProductFilterInput;
+  sort?: GraphQLProductSortInput
+}
+
+export interface GraphQLProductFilterInput {
+  activity: FilterEqualTypeInput;
+  category_gear: FilterEqualTypeInput;
+  category_id: FilterEqualTypeInput;
+  category_uid: FilterEqualTypeInput;
+  category_url_path: FilterEqualTypeInput;
+  climate: FilterEqualTypeInput;
+  collar: FilterEqualTypeInput;
+  color: FilterEqualTypeInput;
+  description: FilterMatchTypeInput;
+  eco_collection: FilterEqualTypeInput;
+  erin_recommends: FilterEqualTypeInput;
+  features_bags: FilterEqualTypeInput;
+  format: FilterEqualTypeInput;
+  gender: FilterEqualTypeInput;
+  material: FilterEqualTypeInput;
+  name: FilterMatchTypeInput;
+  new: FilterEqualTypeInput;
+  pattern: FilterEqualTypeInput;
+  performance_fabric: FilterEqualTypeInput;
+  price: FilterRangeTypeInput;
+  purpose: FilterEqualTypeInput;
+  sale: FilterEqualTypeInput;
+  short_description: FilterMatchTypeInput;
+  size: FilterEqualTypeInput;
+  sku: FilterEqualTypeInput;
+  sleeve: FilterEqualTypeInput;
+  strap_bags: FilterEqualTypeInput;
+  style_bags: FilterEqualTypeInput;
+  style_bottom: FilterEqualTypeInput;
+  style_general: FilterEqualTypeInput;
+  url_key: FilterEqualTypeInput;
+}
+
+export interface FilterEqualTypeInput {
+  eq?: string;
+  in?: string[];
+}
+
+export interface FilterMatchTypeInput {
+  match?: string;
+  match_type?: "FULL" | "PARTIAL";
+}
+
+export interface FilterRangeTypeInput {
+  from?: string;
+  to?: string;
+}
+
+export interface GraphQLProductSortInput {
+  name: "ASC" | "DESC";
+  position: "ASC" | "DESC";
+  price: "ASC" | "DESC";
+  relevance: "ASC" | "DESC";
 }
