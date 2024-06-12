83abc77d7d8926b87df6c13022fef5b24e6af5fa
viktormarinho <viktormpcs@gmail.com>
2024-06-12 10:26:31 -0300

    remove basket hint

--- linx/loaders/cart.ts
+++ linx/loaders/cart.ts
@@ -1,23 +1,9 @@
-import { getCookies, setCookie } from "std/http/cookie.ts";
 import type { AppContext } from "../../linx/mod.ts";
 import { proxySetCookie } from "../../utils/cookie.ts";
 import { toLinxHeaders } from "../utils/headers.ts";
 import { toCart } from "../utils/transform.ts";
 import type { CartResponse } from "../utils/types/basketJSON.ts";
 
-export const DECO_LINX_BASKET_COOKIE = "d-linx-basket";
-
-/**
- * Some Linx stores will need the BasketID for effectively
- * referencing a shopping session.
- * @param headers Incoming request headers
- * @returns
- */
-export function getBasketHint(headers: Headers): number | undefined {
-  const cookies = getCookies(headers);
-  const basketId = cookies[DECO_LINX_BASKET_COOKIE];
-  return basketId ? Number(basketId) : undefined;
-}
 
 /**
  * @title Linx Integration
@@ -29,14 +15,6 @@ const loader = async (
   ctx: AppContext,
 ): Promise<CartResponse | null> => {
   const { api } = ctx;
-
-  const cookies = getCookies(req.headers);
-
-  for (const [a, b] of Object.entries(cookies)) {
-    console.log(
-      a, b
-    );
-  }
 
   const response = await api["POST /web-api/v1/Shopping/Basket/Get"]({}, {
     headers: toLinxHeaders(req.headers),
