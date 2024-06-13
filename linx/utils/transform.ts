97107598175c033a13b5e4e6d88bce36e22f2d73
Italo Barreto <italo@agenciafg.com.br>
2024-06-13 16:38:10 -0300

    add color in product options to product additional property

--- linx/utils/transform.ts
+++ linx/utils/transform.ts
@@ -4,10 +4,11 @@ import {
   Offer,
   Product,
   ProductDetailsPage,
+  PropertyValue,
   UnitPriceSpecification,
 } from "../../commerce/types.ts";
 import { DEFAULT_IMAGE } from "../../commerce/utils/constants.ts";
-import { Cart } from "./types/basketJSON.ts";
+import { CartResponse } from "./types/basketJSON.ts";
 import {
   Facet,
   Navigation as GridProductsNavigation,
@@ -27,6 +28,8 @@ import {
   Item as LinxSuggestionProductJSON,
   Product as LinxSuggestionProductGroupJSON,
 } from "./types/suggestionsJSON.ts";
+import { ProductAuction } from "./types/auctionJSON.ts";
+import { Model as ProductAuctionDetail } from "./types/auctionDetailJSON.ts";
 
 type LinxProductGroup =
   | LinxProductGroupList
@@ -53,54 +56,56 @@ const pickVariant = (variants: LinxProduct[], variantId: number | null) => {
   return variants[0];
 };
 
-const toOffer = (variant: LinxProduct): Offer => {
+const toOffer = (variant: LinxProduct, product: LinxProductGroup): Offer => {
+  const item = variant.Price ? variant : product;
   const priceSpecification: UnitPriceSpecification[] = [
     {
       "@type": "UnitPriceSpecification",
       priceType: "https://schema.org/ListPrice",
-      price: variant.Price?.ListPrice ?? variant.ListPrice ?? Infinity,
+      price: item.Price?.ListPrice ?? item.ListPrice ?? Infinity,
     },
     {
       "@type": "UnitPriceSpecification",
       priceType: "https://schema.org/SalePrice",
-      price: variant.Price?.SalesPrice ?? variant.RetailPrice ?? Infinity,
-    },
-    {
-      "@type": "UnitPriceSpecification",
-      priceType: "https://schema.org/SalePrice",
-      priceComponentType: "https://schema.org/Installment",
-      name: variant.Price?.MaxInstallmentsNoInterest?.PaymentType,
-      description: variant.Price?.MaxInstallmentsNoInterest?.PaymentType,
-      billingDuration: variant.Price?.MaxInstallmentsNoInterest
-        ?.Installments,
-      billingIncrement: variant.Price?.MaxInstallmentsNoInterest
-        ?.InstallmentPrice,
-      price: variant.Price?.MaxInstallmentsNoInterest?.RetailPrice ?? Infinity,
+      price: item.Price?.SalesPrice ?? item.RetailPrice ?? Infinity,
     },
     {
       "@type": "UnitPriceSpecification",
       priceType: "https://schema.org/SalePrice",
       priceComponentType: "https://schema.org/Installment",
-      name: variant.Price?.BestInstallment?.PaymentType,
-      description: variant.Price?.BestInstallment?.PaymentType,
-      billingDuration: variant.Price?.BestInstallment
+      name: item.Price?.MaxInstallmentsNoInterest?.PaymentType,
+      description: item.Price?.MaxInstallmentsNoInterest?.PaymentType,
+      billingDuration: item.Price?.MaxInstallmentsNoInterest
         ?.Installments,
-      billingIncrement: variant.Price?.BestInstallment
+      billingIncrement: item.Price?.MaxInstallmentsNoInterest
         ?.InstallmentPrice,
-      price: variant.Price?.BestInstallment?.RetailPrice ?? Infinity,
+      price: item.Price?.MaxInstallmentsNoInterest?.RetailPrice ?? Infinity,
+    },
+    {
+      "@type": "UnitPriceSpecification",
+      priceType: "https://schema.org/SalePrice",
+      priceComponentType: "https://schema.org/Installment",
+      name: item.Price?.BestInstallment?.PaymentType,
+      description: item.Price?.BestInstallment?.PaymentType,
+      billingDuration: item.Price?.BestInstallment
+        ?.Installments,
+      billingIncrement: item.Price?.BestInstallment
+        ?.InstallmentPrice,
+      price: item.Price?.BestInstallment?.RetailPrice ?? Infinity,
     },
   ];
 
   return {
     "@type": "Offer",
-    seller: variant?.Price?.BuyBox?.SellerName ?? undefined,
-    priceValidUntil: variant?.PromotionTo ?? undefined,
-    price: variant.Price?.SalesPrice ?? variant.RetailPrice ?? Infinity,
+    seller: item?.Price?.BuyBox?.SellerName ?? undefined,
+    priceValidUntil: item?.PromotionTo ?? undefined,
+    price: item.Price?.SalesPrice ?? item.RetailPrice ?? Infinity,
     priceSpecification,
     inventoryLevel: {},
-    availability: variant.Availability != "O"
-      ? "https://schema.org/InStock"
-      : "https://schema.org/OutOfStock",
+    availability:
+      item.Availability != "O" && item.AvailabilityText != "Descontinuado"
+        ? "https://schema.org/InStock"
+        : "https://schema.org/OutOfStock",
   };
 };
 
@@ -125,15 +130,64 @@ export const toProduct = (
     console.info(product.ProductID);
   }
 
-  const offer = toOffer(variant);
+  const offer = toOffer(variant, product);
   const offers = offer ? [offer] : [];
 
-  const additionalProperty = variant.SKUOptions?.map((option) => ({
+  const skuOptions: PropertyValue[] = variant.SKUOptions?.map((option) => ({
     "@type": "PropertyValue" as const,
     name: option.Name,
     value: option.Title,
     propertyID: option.Value,
-  })) ?? [];
+    additionalType: "skuOptions",
+  }));
+
+  const prodOptions: PropertyValue[] = product.Options.map(option => {
+    return option.Values.map(optValue => {
+      const imagePath = optValue.ImagePath 
+        ? `/Custom/Content/Swatches${optValue.ImagePath}`
+        : null;
+      const url = imagePath
+        ? new URL(imagePath, cdn).href
+        : "";
+
+      const color = optValue.Color ? optValue.Color : "";
+
+      return {
+        "@type": "PropertyValue" as const,
+        name: option.Label,
+        value: optValue.Value,
+        image: [{
+          "@type": "ImageObject" as const,
+          url,
+        }],
+        color: color,
+        additionalType: "prodOptions",
+      }
+    });
+  }).flat();
+
+  const metadatas: PropertyValue[] = product.ExtendedMetadatas?.map((item) => ({
+    "@type": "PropertyValue" as const,
+    name: item.Alias,
+    value: item.Value,
+    propertyID: item.PropertyMetadataID.toString(),
+    additionalType: "metadatas",
+  }));
+
+  const descriptions: PropertyValue[] = product.Descriptions?.map((desc) => ({
+    "@type": "PropertyValue" as const,
+    name: desc.Name,
+    alternateName: desc.Alias,
+    value: desc.Value,
+    additionalType: "descriptions",
+  }));
+
+  const additionalProperty = [
+    ...skuOptions,
+    ...metadatas,
+    ...descriptions,
+    ...prodOptions,
+  ];
 
   const hasVariant = level < 1
     ? variants.map((variant) =>
@@ -174,6 +228,7 @@ export const toProduct = (
     "@type": "Product",
     productID: `${variant.ProductID}`,
     sku: `${variant.SKU}`,
+    identifier: `${product.SKU}`,
     url: productURL.href,
     name: variant.Name,
     gtin: product.Uom,
@@ -192,6 +247,7 @@ export const toProduct = (
       description: product.ShortDescription,
       image: groupImages,
       productGroupID: product.ProductID.toString(),
+      identifier: `${product.IntegrationID}`,
       additionalProperty: [],
       hasVariant,
     },
@@ -264,21 +320,51 @@ export const toProductDetails = (
   };
 };
 
-export const toCart = (cart: Cart, { cdn }: { cdn: string }): Cart | null => {
-  if (!cart.Basket?.IsValid) {
+export const toCart = (
+  cart: CartResponse,
+  { cdn }: { cdn: string },
+): CartResponse | null => {
+  if (!cart.IsValid) {
     return null;
   }
 
   return ({
     ...cart,
-    Model: undefined,
-    PageInfo: undefined,
-    Basket: {
-      ...cart.Basket,
-      Items: cart.Basket.Items?.map((item) => ({
-        ...item,
-        ImagePath: new URL(item.ImagePath || "", cdn).href,
-      })),
+    Shopper: {
+      ...cart.Shopper,
+      Basket: {
+        ...cart.Shopper.Basket,
+        Items: cart.Shopper.Basket.Items?.map((item) => ({
+          ...item,
+          ImagePath: new URL(item.ImagePath || "", cdn).href,
+        })),
+      },
+    },
+  });
+};
+
+export const toAuction = (
+  auction: ProductAuction,
+  { cdn }: { cdn: string },
+): ProductAuction => {
+  return ({
+    ...auction,
+    ProductImage: new URL(auction.ProductImage, cdn).href,
+  });
+};
+
+export const toAuctionDetail = (
+  auction: ProductAuctionDetail,
+  { cdn: _cdn }: { cdn: string },
+): ProductAuctionDetail => {
+  // TODO: Fix image URLs
+  return ({
+    ...auction,
+    Product: {
+      ...auction.Product,
+      Medias: [
+        ...auction.Product.Medias,
+      ],
     },
   });
 };
