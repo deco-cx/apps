93e9e708ac9d6006a15d60c271bc0afd75d472ff
Luis Sousa <lv-ss@hotmail.com>
2024-06-13 12:39:41 -0300

    sh: add  missing hotsite params

--- smarthint/loaders/productListingPage.ts
+++ smarthint/loaders/productListingPage.ts
@@ -3,26 +3,64 @@ import { AppContext } from "../mod.ts";
 import { toFilters, toProduct, toSortOption } from "../utils/transform.ts";
 import { redirect } from "deco/mod.ts";
 import { getSessionCookie } from "../utils/getSession.ts";
+import { Filter, SearchSort } from "./searchListPage.ts";
+
+export interface Props {
+  /**
+   * @description Number of products that must be returned per page
+   */
+  size: number;
+  /**
+   * @hide
+   */
+  from?: number;
+  /**
+   * @hide
+   */
+  filter?: Filter[];
+  /**
+   * @hide
+   */
+  searchSort?: SearchSort;
+}
 
 /**
  * @title Smarthint Integration - Hotsite
  * @description Product List Page
  */
 const loader = async (
-  _props: unknown,
+  props: Props,
   req: Request,
   ctx: AppContext,
 ): Promise<ProductListingPage | null> => {
   const { api, shcode, cluster } = ctx;
+  const { size = 12, from: fromParam = 0, filter = [], searchSort } = props;
 
   const url = new URL(req.url);
+
+  const page = Number(url.searchParams.get("page") ?? 1);
+  const from = fromParam ?? page <= 1 ? 0 : (page - 1) * size;
+
+  const sort = url.searchParams.get("sort") ??
+    url.searchParams.get("searchSort") ?? searchSort;
+
+  const filters = url.searchParams.getAll("filter").length
+    ? url.searchParams.getAll("filter")
+    : filter.length
+    ? filter.map((filterItem) => `${filterItem.field}:${filterItem.value}`)
+    : undefined;
+
   const anonymous = getSessionCookie(req.headers);
 
   const data = await api["GET /:cluster/hotsite"]({
     cluster,
     shcode,
     anonymous,
-    url: url.pathname,
+    url: url.pathname.replace("/", ""),
+    size,
+    from,
+    searchSort: String(sort),
+    filter: filters,
   }).then((r) => r.json());
 
   if (data.SearchResult?.IsRedirect) {
@@ -37,7 +75,22 @@ const loader = async (
 
   const sortOptions = toSortOption(data.SearchResult?.Sorts ?? []);
 
-  const filters = toFilters(data.SearchResult?.Filters ?? [], url);
+  const resultFilters = toFilters(data.SearchResult?.Filters ?? [], url);
+
+  const hasNextPage = (data?.SearchResult?.TotalResult ?? 0) > size;
+  const hasPreviousPage = from > 0 &&
+    (data?.SearchResult?.TotalResult ?? 0) > size;
+
+  const nextPage = new URLSearchParams(url.searchParams);
+  const previousPage = new URLSearchParams(url.searchParams);
+
+  if (hasNextPage) {
+    nextPage.set("page", (page + 1).toString());
+  }
+
+  if (hasPreviousPage) {
+    previousPage.set("page", (page - 1).toString());
+  }
 
   return {
     "@type": "ProductListingPage",
@@ -48,13 +101,13 @@ const loader = async (
       itemListElement: [],
       numberOfItems: 0,
     },
-    filters,
+    filters: resultFilters,
     pageInfo: {
-      records: data.SearchResult?.TotalResult,
-      recordPerPage: data.SearchResult?.TotalResult,
-      nextPage: undefined,
-      previousPage: undefined,
-      currentPage: 0,
+      records: data?.SearchResult?.TotalResult,
+      recordPerPage: size,
+      nextPage: hasNextPage ? `?${nextPage}` : undefined,
+      previousPage: hasPreviousPage ? `?${previousPage}` : undefined,
+      currentPage: page,
       pageTypes: [
         "Cluster",
       ],
