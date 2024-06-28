ff46f44402a61f1ddecbfebf0af964acfcbfa658
viktormarinho <viktormpcs@gmail.com>
2024-06-28 11:49:59 -0300

    test removing proxy failing headers from all requests

--- linx/utils/headers.ts
+++ linx/utils/headers.ts
@@ -1,5 +1,12 @@
 import { removeCFHeaders } from "../../website/handlers/proxy.ts";
 
+export const linxProxyFailingHeaders = [
+  "x-b3-sampled",
+  "x-b3-spanid",
+  "x-b3-traceid",
+  "x-envoy-attempt-count",
+];
+
 /**
  * Some headers can cause the linx API to error, so we always filter
  * only the necessary ones when proxying request headers to the linx API
@@ -8,6 +15,10 @@ export function toLinxHeaders(inputHeaders: Headers): Headers {
   const headers = new Headers(inputHeaders);
 
   removeCFHeaders(headers);
+
+  for (const header of linxProxyFailingHeaders) {
+    headers.delete(header);
+  }
 
   return headers;
 }
