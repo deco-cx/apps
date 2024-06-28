6b458072e8bc7b37bfdc94fde9ab7a687c778187
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-06-28 20:30:00 -0300

    Fix dirty cookies on vtex http client

--- utils/normalize.ts
+++ utils/normalize.ts
@@ -19,3 +19,19 @@ export const removeNonAscChars = (str: string): string => {
   const latin1Regex = /[^\x00-\x7F]|[\"\']/g;
   return str.replace(latin1Regex, "");
 };
+
+export const removeDirtyCookies = (headers?: Headers) => {
+  if (!headers) {
+    return;
+  }
+  const existingCookie = removeNonAscChars(headers.get("cookie") || "");
+  // remove only brackets
+  const removedBrackets = existingCookie?.replace(/[\[\]]/g, "").normalize(
+    "NFD",
+  ).replace(
+    /[\u0300-\u036f]/g,
+    "",
+  );
+  headers.set("cookie", `${removedBrackets}`);
+  return headers;
+};
