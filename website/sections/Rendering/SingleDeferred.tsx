8b239bbf44435728b93c714563147a706cf03e48
igorbrasileiro <brasileiro456@gmail.com>
2024-07-08 15:33:36 -0300

    Improve types

--- website/sections/Rendering/SingleDeferred.tsx
+++ website/sections/Rendering/SingleDeferred.tsx
@@ -1,31 +1,18 @@
 import type { Section } from "deco/blocks/section.ts";
-import PartialDefered, {
-  type Props as BasePartialDeferredProps,
-} from "./Deferred.tsx";
-import HTMXDefered, {
-  type Props as BaseHTMXDeferredProps,
-} from "../../../htmx/sections/Deferred.tsx";
+import PartialDefered from "./Deferred.tsx";
+import HTMXDefered from "../../../htmx/sections/Deferred.tsx";
 import type { AppContext } from "../../mod.ts";
-import { asResolved } from "deco/mod.ts";
+import { asResolved, isDeferred } from "deco/mod.ts";
 import { __DECO_FBT } from "../../handlers/fresh.ts";
-
-type DeferredSection = () => Promise<Section>;
-
-type PartialDeferredProps = Omit<BasePartialDeferredProps, "sections">;
-type HTMXDeferredProps = Omit<BaseHTMXDeferredProps, "sections">;
-
-const isPartial = (
-  props: SectionProps,
-): props is PartialDeferredProps & { framework: "fresh"; section: Section } =>
-  props.framework === "fresh";
 
 const isHtmx = (
   props: SectionProps,
-): props is HTMXDeferredProps & { framework: "htmx"; section: Section } =>
-  props.framework === "htmx";
+): props is SectionProps & { framework: "htmx" } => props.framework === "htmx";
 
-interface Props extends HTMXDeferredProps, PartialDeferredProps {
+type Trigger = "load" | "intersect";
+interface Props {
   section: Section;
+  trigger?: Trigger;
 }
 
 export const loader = async (props: Props, _req: Request, ctx: AppContext) => {
@@ -37,10 +24,8 @@ export const loader = async (props: Props, _req: Request, ctx: AppContext) => {
     ctx.isBot;
   const framework = ctx.flavor?.framework || "fresh";
 
-  if (disableDefer) {
-    const section =
-      await (props.section as unknown as DeferredSection | undefined)?.() ??
-        null;
+  if (disableDefer && isDeferred(props.section)) {
+    const section = await props.section() as Section | null;
     return { ...props, framework, section };
   }
 
@@ -53,22 +38,23 @@ function SingleDeferred(props: SectionProps) {
   if (isHtmx(props)) {
     return (
       <HTMXDefered
-        {...props}
         sections={props.section ? [props.section] : []}
-        loading={props.section ? "eager" : props.loading}
+        loading={props.section ? "eager" : "lazy"}
+        trigger={{
+          type: "load",
+        }}
       />
     );
   }
 
   return (
     <PartialDefered
-      {...props}
       sections={props.section ? [props.section] : []}
-      display={props.section
-        ? true
-        : isPartial(props)
-        ? props.display
-        : undefined}
+      display={props.section ? true : false}
+      behavior={{
+        type: "scroll",
+        payload: 0,
+      }}
     />
   );
 }
