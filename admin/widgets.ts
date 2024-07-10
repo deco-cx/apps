fa121cf87da3ddf5cc3b41db78ab73601ae26af6
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-10 19:35:16 -0300

    feat: new types added to widgets
    adiciona novos tipos ao arquivo widgets.ts, aprimorando a usabilidade dos Widgets da Deco para desenvolvedores. Com essas mudanças, os usuários podem importar diretamente os tipos em seus códigos, simplificando o desenvolvimento e a manutenção de widgets.

--- admin/widgets.ts
+++ admin/widgets.ts
@@ -23,3 +23,37 @@ export type SiteRoute = string;
  * @format map
  */
 export type MapWidget = string;
+
+/**
+ * @format textarea
+ */
+export type TextArea = string;
+
+/**
+ * @format rich-text
+ */
+export type RichText = string;
+
+/**
+ * @format color-input
+ * @default #000000
+ */
+export type ColorInput = string;
+
+/**
+ * @format code
+ * @language css
+ */
+export type CSSCode = string;
+
+/**
+ * @format code
+ * @language typescript
+ */
+export type TypescriptCode = string;
+
+/**
+ * @format code
+ * @language json
+ */
+export type JSONCode = string;
