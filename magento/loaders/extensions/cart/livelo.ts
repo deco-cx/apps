e0b70d9a55f35f424cb0c3400d5a79ac1c7911ef
Lucas Sacci <sacci@carcara.tech>
2024-07-15 18:56:42 -0300

    fix(magento): extension on cart

---
+++ magento/loaders/extensions/cart/livelo.ts
@@ -0,0 +1,27 @@
+import { ExtensionOf } from "../../../../website/loaders/extension.ts";
+import { AppContext } from "../../../mod.ts";
+import { ExtensionProps } from "../../../utils/client/types.ts";
+import { liveloCartExt } from "../../../utils/extension.ts";
+import { Cart } from "../../cart.ts";
+
+/**
+ * @title Magento ExtensionOf - Cart
+ * @description Add extra data to your loader. This may harm performance
+ */
+const loader = (
+    props: ExtensionProps,
+    _req: Request,
+    ctx: AppContext,
+): ExtensionOf<Cart | null> =>
+async (page: Cart | null) => {
+    if (!page) {
+        return page;
+    }
+
+    if (props.active) {
+        return await liveloCartExt(page, props.path, ctx);
+    }
+    return page;
+};
+
+export default loader;
