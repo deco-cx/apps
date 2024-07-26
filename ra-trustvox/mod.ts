trustvox/deco-apps

remove global state and add inline loaders to get the state

ra-trustvox/mod.ts
@@ -23,8 +23,6 @@ export interface Props {
   enableStaging?: boolean;
 }
 
-export let state: Props;
-
 /**
  * @title RA Trustvox
  * @description RA trustvox reviews test.
@@ -32,10 +30,8 @@ export let state: Props;
  * @logo https://raw.githubusercontent.com/trustvox/deco-apps/enhancement/trustvox-app/ra-trustvox/ra-trustvox.png
  */
 export default function RATrustvox(
-  props: Props,
+  state: Props,
 ): App<Manifest, Props> {
-  state = props;
-
   return { manifest, state };
 }
 

deco-cx/apps

ra-trustvox/mod.ts
@@ -23,8 +23,6 @@ export interface State {
   enableStaging?: boolean;
 }
 
-export let state: State;
-
 /**
  * @title RA Trustvox
  * @description RA trustvox reviews.
@@ -32,10 +30,8 @@ export let state: State;
  * @logo https://raw.githubusercontent.com/trustvox/deco-apps/enhancement/trustvox-app/ra-trustvox/ra-trustvox.png
  */
 export default function RATrustvox(
-  props: State,
+  state: State,
 ): App<Manifest, State> {
-  state = props;
-
   return { manifest, state };
 }
 