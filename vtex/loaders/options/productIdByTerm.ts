9dee242936e173f8e2315510c641083d62a5a7d4
guitavano <tavano62@gmail.com>
2024-07-29 15:28:03 -0300

    fix empty search

--- vtex/loaders/options/productIdByTerm.ts
+++ vtex/loaders/options/productIdByTerm.ts
@@ -2,37 +2,28 @@ import { allowCorsFor } from "deco/mod.ts";
 import { AppContext } from "../../mod.ts";
 
 interface Props {
-    term?: string;
+  term?: string;
 }
 
 const loader = async (
-    props: Props,
-    req: Request,
-    ctx: AppContext,
+  props: Props,
+  req: Request,
+  ctx: AppContext,
 ) => {
+  Object.entries(allowCorsFor(req)).map(([name, value]) => {
+    ctx.response.headers.set(name, value);
+  });
 
-    Object.entries(allowCorsFor(req)).map(([name, value]) => {
-        ctx.response.headers.set(name, value);
-      });
+  const suggestions = await ctx.invoke.vtex.loaders.intelligentSearch
+    .suggestions({
+      query: props.term || "",
+      count: 6,
+    });
 
-    const suggestions = await ctx.invoke.vtex.loaders.intelligentSearch.suggestions({
-        query: props.term || "a",
-        count: 6
-    })
-
-
-    if(suggestions?.products?.length === 0) {
-        return [{
-            value: "No products found, use search",
-            label: "No products found, use search"
-        }]
-    }
-
-    return suggestions?.products?.map((product) => ({
-        value: `${product.productID}`,
-        label: `${product.productID} - ${product.name}`,
-      }))
-
-}
+  return suggestions?.products?.map((product) => ({
+    value: `${product.productID}`,
+    label: `${product.productID} - ${product.name}`,
+  }));
+};
 
 export default loader;
