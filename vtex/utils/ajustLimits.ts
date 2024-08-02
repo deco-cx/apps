a64327776806d36201550120880696538eb2b5cb
Lucas Sacci <sacci@carcara.tech>
2024-08-02 18:22:29 -0300

    linting

--- vtex/utils/ajustLimits.ts
+++ vtex/utils/ajustLimits.ts
@@ -1,13 +1,13 @@
 export function ajustLimits(
-    start: number,
-    end: number,
-    offset: number,
+  start: number,
+  end: number,
+  offset: number,
 ) {
-    const newStart = start > 0 ? start : 0;
-    const newEnd = (end - newStart > offset) ? newStart + offset : end;
+  const newStart = start > 0 ? start : 0;
+  const newEnd = (end - newStart > offset) ? newStart + offset : end;
 
-    return {
-        start: newStart,
-        end: newEnd,
-    };
+  return {
+    start: newStart,
+    end: newEnd,
+  };
 }
