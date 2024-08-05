d6c8d2e16908e48267edb84adca235648646a8a8
Jonas Di Pierro <j.dipierro@socialdigitalcommerce.com.br>
2024-08-05 09:59:03 -0300

    configuring myVtex app context to use in reviews and ratings POST req

--- vtex/loaders/config.ts
+++ vtex/loaders/config.ts
@@ -4,17 +4,20 @@ import { type AppContext } from "../mod.ts";
 import { type SP } from "../utils/client.ts";
 import { type OpenAPI as API } from "../utils/openapi/api.openapi.gen.ts";
 import { type OpenAPI as VCS } from "../utils/openapi/vcs.openapi.gen.ts";
+import { type OpenAPI as MY } from "../utils/openapi/my.openapi.gen.ts";
 
 export type Config = {
   sp: ReturnType<typeof createHttpClient<SP>>;
   io: ReturnType<typeof createGraphqlClient>;
   vcs: ReturnType<typeof createHttpClient<VCS>>;
+  my: ReturnType<typeof createHttpClient<MY>>;
   api: ReturnType<typeof createHttpClient<API>>;
 };
 
 const loader = (_props: unknown, _req: Request, ctx: AppContext): Config => ({
   sp: ctx.sp,
   io: ctx.io,
+  my: ctx.my,
   vcs: ctx.vcs,
   api: ctx.api,
 });
