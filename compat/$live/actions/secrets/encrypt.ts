c93a85180c9334b39045f967592d16921877190b
viktormarinho <viktormpcs@gmail.com>
2024-08-15 10:25:07 -0300

    fix weird import

--- compat/$live/actions/secrets/encrypt.ts
+++ compat/$live/actions/secrets/encrypt.ts
@@ -1,1 +1,2 @@
 export * from "../../../../website/actions/secrets/encrypt.ts";
+export { default } from "../../../../website/actions/secrets/encrypt.ts";
--- compat/$live/mod.ts
+++ compat/$live/mod.ts
@@ -1,45 +1,15 @@
 // deno-lint-ignore-file no-explicit-any
 import type { App } from "deco/mod.ts";
-// this should not be changed to $live because newly created versions might not include live.gen.ts.
-import { buildImportMap } from "deco/blocks/utils.tsx";
 
-import type { Manifest as WebSiteManifest } from "../../website/manifest.gen.ts";
 import webSite, { Props } from "../../website/mod.ts";
-import type { Manifest as WorkflowsManifest } from "../../workflows/manifest.gen.ts";
 
-import { AppContext as AC, ImportMap } from "deco/blocks/app.ts";
+import { AppContext as AC } from "deco/blocks/app.ts";
 import workflows from "../../workflows/mod.ts";
-import manifest, { Manifest as _Manifest } from "./manifest.gen.ts";
+import manifest, { Manifest } from "./manifest.gen.ts";
 
 export { onBeforeResolveProps } from "../../website/mod.ts";
-export type AppContext = AC<ReturnType<typeof App>>;
-export type Manifest = _Manifest;
 
-export type ManifestMappings = Partial<
-  {
-    [
-      blockType in keyof Omit<
-        Manifest,
-        "name" | "baseUrl" | "routes" | "islands"
-      >
-    ]: Partial<
-      {
-        [
-          blockKey in keyof Omit<
-            Manifest,
-            "name" | "baseUrl" | "routes" | "islands"
-          >[blockType]
-        ]: blockType extends keyof (WebSiteManifest | WorkflowsManifest)
-          ? keyof (WebSiteManifest[blockType] & WorkflowsManifest[blockType])
-          : blockType extends keyof WebSiteManifest
-            ? keyof (WebSiteManifest[blockType])
-          : blockType extends keyof WorkflowsManifest
-            ? keyof (WorkflowsManifest[blockType])
-          : never;
-      }
-    >;
-  }
->;
+export type AppContext = AC<ReturnType<typeof App>>;
 
 export type { Props };
 /**
@@ -54,30 +24,9 @@ export default function App(
 > {
   const { resolvables: _ignoreResolvables, ...webSiteApp } = webSite(state);
   const workflowsApp = workflows({});
-  const webSiteManifest = webSiteApp.manifest;
-  const workflowsManifest = workflowsApp.manifest;
-  const webSiteManifestImportMap = buildImportMap(webSiteManifest);
-  const workflowsManifestImportMap = buildImportMap(workflowsManifest);
-  const importMap: ImportMap = {
-    ...webSiteManifestImportMap,
-    ...workflowsManifestImportMap,
-    imports: {
-      ...webSiteManifestImportMap.imports,
-      ...workflowsManifestImportMap.imports,
-    },
-  };
 
-  const liveImportMap = buildImportMap(manifest);
   return {
     state,
-    importMap: {
-      ...liveImportMap,
-      ...importMap,
-      imports: {
-        ...liveImportMap.imports,
-        ...importMap.imports,
-      },
-    },
     manifest,
     dependencies: [webSiteApp, workflowsApp],
   };
