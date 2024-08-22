348b05e3f66f31f6e3c0f50a68f1df5b6e3ccb3b
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-08-22 17:34:25 -0300

    Fix: LivePage exports (#798)

--- compat/$live/pages/LivePage.tsx
+++ compat/$live/pages/LivePage.tsx
@@ -1,4 +1,21 @@
-export * from "../../../website/pages/Page.tsx";
-import livepageDefault from "../../../website/pages/Page.tsx";
+import {
+  default as livepageDefault,
+  type DefaultPathProps,
+  loader,
+  pageIdFromMetadata,
+  Preview,
+  type Props,
+  renderSection,
+  type Sections,
+} from "../../../website/pages/Page.tsx";
 
 export default livepageDefault;
+export {
+  DefaultPathProps,
+  loader,
+  pageIdFromMetadata,
+  Preview,
+  Props,
+  renderSection,
+  Sections,
+};
