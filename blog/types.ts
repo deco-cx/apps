6b2b6c8cc571201bf7ef18fd76f28be14ce2bec1
decobot <capy@deco.cx>
2024-09-04 17:58:10 -0300

    feat(blogpost): BlogPosting type added

--- blog/types.ts
+++ blog/types.ts
@@ -1,5 +1,10 @@
 import { ImageWidget } from "../admin/widgets.ts";
 import { ImageObject, PageInfo, Thing } from "../commerce/types.ts";
+//Vira about
+export interface Category {
+  name: string;
+  slug: string;
+}
 
 /**
  * @titleBy name
@@ -8,18 +13,46 @@ import { ImageObject, PageInfo, Thing } from "../commerce/types.ts";
  */
 export type Author = Person | Organization;
 
-//Vira about
-export interface Category {
-  name: string;
-  slug: string;
-}
-
-export interface BlogPosting extends Omit<Thing, "@type">{
-  "@type": "BlogPosting", 
+//TODO: Comments (https://schema.org/Comment) and aggreggate rating (https://schema.org/AggregateRating)
+export interface BlogPosting extends Omit<Thing, "@type" | "description"> {
+  "@type": "BlogPosting";
+  /** @description Headline of the article. */
+  headline: string;
+  /**
+   * @description Date of first publication or broadcast, in ISO 8601 date format (Chapter 5.4).
+   */
+  datePublished: string;
+  /**
+   * @description The date on which the article was most recently modified, in ISO 8601 date format (Chapter 5.4).
+   */
+  dateModified: string;
+  /** The author of this content */
+  author: Author;
+  /**
+   * An abstract is a short description that summarizes the article.
+   */
+  abstract: string;
+  /**
+   * The actual body of the article.
+   * @format rich-text
+   */
+  articleBody: string;
+  /**
+   * The subject matter of the content.
+   * @description Put categories here
+   */
+  about?: Array<About>;
+  /** Keywords or tags used to describe some item. */
+  keywords?: Array<string>;
+  /** The number of words in the text of the Article. */
+  wordCount?: number;
+  /** Conditions that affect the availability of, or method(s) of access to, an item. */
+  conditionsOfAccess?: string;
+  /** Approximate or typical time it usually takes to work with or through the content of this work for the typical or target audience. */
+  timeRequired?: string;
 }
 
 export interface BlogPost {
-  
   title: string;
   excerpt: string;
   image?: ImageWidget;
@@ -93,10 +126,6 @@ export interface Person extends Omit<Thing, "@type"> {
   "@type": "Person";
   /** Email address. */
   email?: string;
-  /** Given name. In the U.S., the first name of a Person. */
-  givenName?: string;
-  /** Family name. In the U.S., the last name of a Person. */
-  familyName?: string;
   /** Gender of something, typically a Person, but possibly also fictional characters, animals, etc */
   gender?: "https://schema.org/Male" | "https://schema.org/Female";
   /** An image of the item. This can be a URL or a fully described ImageObject. **/
