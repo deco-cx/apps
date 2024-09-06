04d114e90dd1fa5b692eedc7a69dd945d1c36d57
Samuel-Brito19 <sambriton7@gmail.com>
2024-09-06 11:16:22 -0300

    Changing deno version

--- linx/utils/types/shared-types/browsingImage.ts
+++ linx/utils/types/shared-types/browsingImage.ts
@@ -1,17 +1,16 @@
-
 export interface BrowsingImage {
-    Attributes: unknown[];
-    HasMediaPath: boolean;
-    Height: number;
-    Index: number;
-    MaxHeight: number;
-    MaxWidth: number;
-    MediaPath: string;
-    MediaSizeType: string;
-    MediaType: string;
-    ProductPath: string;
-    Title: null;
-    Url: null;
-    VariationPath: string;
-    Width: number;
-  }
+  Attributes: unknown[];
+  HasMediaPath: boolean;
+  Height: number;
+  Index: number;
+  MaxHeight: number;
+  MaxWidth: number;
+  MediaPath: string;
+  MediaSizeType: string;
+  MediaType: string;
+  ProductPath: string;
+  Title: null;
+  Url: null;
+  VariationPath: string;
+  Width: number;
+}
