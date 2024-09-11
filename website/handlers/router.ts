af5a50188515e138b569de28cceed303fce54c50
guitavano <tavano62@gmail.com>
2024-09-11 15:14:11 -0300

    only add search if we have search

--- website/handlers/router.ts
+++ website/handlers/router.ts
@@ -87,7 +87,7 @@ export const router = (
         }
         return new URLPattern({
           pathname: url.pathname,
-          search: url.search,
+          ...(url.search ? {search: url.search} : {}),
         });
       })();
       const res = pattern.exec(req.url);
