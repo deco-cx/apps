ca6d2a7f7058f76078d65e28b771f133804f7ff2
Gabriel Antiqueira <mantiqueira@deco.cx>
2024-09-13 12:10:09 -0300

    Remove default values | Add Preview

--- sap/mod.ts
+++ sap/mod.ts
@@ -1,6 +1,8 @@
 import type { App, FnContext } from "deco/mod.ts";
 import { createHttpClient } from "../utils/http.ts";
 import manifest, { Manifest } from "./manifest.gen.ts";
+import { PreviewContainer } from "../utils/preview.tsx";
+import { Markdown } from "../decohub/components/Markdown.tsx";
 import { API } from "./utils/client/client.ts";
 
 export type AppContext = FnContext<State, Manifest>;
@@ -9,13 +11,11 @@ export type AppContext = FnContext<State, Manifest>;
 export interface Props {
   /**
    * @title Api url
-   * @default https://apolloapi.electrolux.com/occ/v2/
    */
   apiUrl: string;
 
   /**
    * @title Base site ID
-   * @default frigidaire
    */
   baseSiteId: string;
 }
@@ -46,3 +46,26 @@ export default function SAP(props: Props): App<Manifest, State> {
     manifest,
   };
 }
+export const preview = async () => {
+  const markdownContent = await Markdown(
+    new URL("./README.md", import.meta.url).href,
+  );
+  return {
+    Component: PreviewContainer,
+    props: {
+      name: "SAP Commerce",
+      owner: "deco.cx",
+      description:
+        "Loaders, actions and workflows for adding SAP Commerce Platform to your website.",
+      logo:
+        "https://www.sap.com/dam/application/shared/logos/sap-logo-svg.svg/sap-logo-svg.svg",
+      images: [],
+      tabs: [
+        {
+          title: "About",
+          content: markdownContent(),
+        },
+      ],
+    },
+  };
+};
