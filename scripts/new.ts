29e3966f9ea91217842a7c155e63798b7731878d
guitavano <tavano62@gmail.com>
2024-09-23 08:03:43 -0300

    fix script to create a new app

--- scripts/new.ts
+++ scripts/new.ts
@@ -6,19 +6,15 @@ const appPath = join(Deno.cwd(), appName);
 const decoTsPath = join(Deno.cwd(), "deco.ts");
 const decoTs = await Deno.readTextFile(decoTsPath);
 
-// Criar a pasta do novo app
 await Deno.mkdir(appPath);
 
-// Clonar o repositório para a nova pasta criada
 const gitClone = Deno.run({
-  cmd: ["git", "clone", repoUrl, appPath],
+  cmd: ["git", "clone", "--depth", "1", repoUrl, appPath],
 });
 
-// Aguarda o comando de clonagem terminar
 await gitClone.status();
 gitClone.close();
 
-// Atualizar o arquivo deco.ts com o novo app
 await Deno.writeTextFile(
   decoTsPath,
   decoTs.replace(`  apps: [`, `  apps: [\n    app("${appName}"),`)
@@ -26,3 +22,6 @@ await Deno.writeTextFile(
 
 const denoJsonPath = join(appPath, "deno.json");
 await Deno.remove(denoJsonPath);
+
+const gitDirPath = join(appPath, ".git");
+await Deno.remove(gitDirPath, { recursive: true });
