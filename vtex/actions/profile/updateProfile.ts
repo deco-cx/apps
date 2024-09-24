3c9a9df3f4459c5a937c5ff5d595dc9c6a593e38
devartes <anacavalcante731@gmail.com>
2024-09-24 16:53:05 -0300

    feat: add updateProfile

---
+++ vtex/actions/profile/updateProfile.ts
@@ -0,0 +1,74 @@
+import { AppContext } from "../../mod.ts";
+import { parseCookie } from "../../utils/vtexId.ts";
+import User from "../../loaders/user.ts";
+
+interface UpdateProfileInfo {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  homePhone?: string | null;
+  gender?: string | null;
+  birthDate?: string | null;
+  corporateName?: string | null;
+  tradeName?: string | null;
+  businessPhone?: string | null;
+  isCorporate?: boolean;
+}
+
+const loader = async (
+  _props: unknown,
+  req: Request,
+  ctx: AppContext,
+  updateInfo: UpdateProfileInfo,
+): Promise<typeof User | null> => {
+  const { io } = ctx;
+  const { cookie, payload } = parseCookie(req.headers, ctx.account);
+
+  if (!payload?.sub || !payload?.userId) {
+    return null;
+  }
+
+  const mutation = `
+    mutation UpdateProfile($input: ProfileInput!) {
+      updateProfile(fields: $input) @context(provider: "vtex.store-graphql") {
+        cacheId
+        firstName
+        lastName
+        birthDate
+        gender
+        homePhone
+        businessPhone
+        document
+        email
+        tradeName
+        corporateName
+        corporateDocument
+        stateRegistration
+        isCorporate
+      }
+    }
+  `;
+
+  try {
+    const { updateProfile: updatedUser } = await io.query<typeof User>(
+      {
+        query: mutation,
+        operationName: "UpdateProfile",
+        variables: {
+          input: updateInfo,
+        },
+      },
+      {
+        headers: {
+          cookie,
+        },
+      },
+    );
+    return updatedUser;
+  } catch (error) {
+    console.error("Error updating user profile:", error);
+    return null;
+  }
+};
+
+export default loader;
