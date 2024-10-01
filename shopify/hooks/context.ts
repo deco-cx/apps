2ce2c165213b70a64e96f38e3b823d4bcf38985f
@yuri_assuncx <killerkoll2012@gmail.com>
2024-10-01 13:19:01 -0300

    [Shopify] Feat: add user (#890)
    * feat(vnda): create addItems action
    
    * feat(shopify): user now exists on shopify integration
    
    * feat: add signIn action
    
    * Revert "feat(vnda): create addItems action"
    
    This reverts commit 13f35d5bb1a21c6cb72aa9512ae586180aac27e1.

--- shopify/hooks/context.ts
+++ shopify/hooks/context.ts
@@ -1,15 +1,18 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { signal } from "@preact/signals";
+import { Person } from "../../commerce/types.ts";
 import { invoke } from "../runtime.ts";
 import type { CartFragment } from "../utils/storefront/storefront.graphql.gen.ts";
 
 export interface Context {
+  user: Person | null;
   cart: CartFragment | null;
 }
 
 const loading = signal<boolean>(true);
 const context = {
-  cart: signal<CartFragment | null>(null),
+  user: IS_BROWSER && signal<Person | null>(null) || { value: null },
+  cart: IS_BROWSER && signal<CartFragment | null>(null) || { value: null },
 };
 
 let queue = Promise.resolve();
@@ -25,11 +28,12 @@ const enqueue = (
 
   queue = queue.then(async () => {
     try {
-      const { cart } = await cb(controller.signal);
+      const { user, cart } = await cb(controller.signal);
 
       controller.signal.throwIfAborted();
 
-      context.cart.value = cart ? { ...context.cart.value, ...cart } : null;
+      context.cart.value = cart || context.cart.value;
+      context.user.value = user || context.user.value;
 
       loading.value = false;
     } catch (error) {
@@ -48,6 +52,7 @@ const enqueue = (
 const load = (signal: AbortSignal) =>
   invoke({
     cart: invoke.shopify.loaders.cart(),
+    user: invoke.shopify.loaders.user(),
   }, { signal });
 
 if (IS_BROWSER) {
--- shopify/mod.ts
+++ shopify/mod.ts
@@ -6,7 +6,9 @@ import type { Secret } from "../website/loaders/secret.ts";
 import { PreviewContainer } from "../utils/preview.tsx";
 import { Markdown } from "../decohub/components/Markdown.tsx";
 import { type App, type FnContext } from "@deco/deco";
+
 export type AppContext = FnContext<State, Manifest>;
+
 /** @title Shopify */
 export interface Props {
   /**
@@ -32,17 +34,21 @@ export interface Props {
    */
   platform: "shopify";
 }
+
 export interface Address {
   provinceCode: string;
 }
+
 export interface AddressLocator {
   byZipCode(zip: string): Promise<Address | null>;
 }
+
 export interface State extends Props {
   storefront: ReturnType<typeof createGraphqlClient>;
   admin: ReturnType<typeof createGraphqlClient>;
   address: AddressLocator;
 }
+
 export const color = 0x96BF48;
 /**
  * @title Shopify
@@ -52,9 +58,11 @@ export const color = 0x96BF48;
  */
 export default function App(props: Props): App<Manifest, State> {
   const { storeName, storefrontAccessToken, adminAccessToken } = props;
+
   const stringAdminAccessToken = typeof adminAccessToken === "string"
     ? adminAccessToken
     : adminAccessToken?.get?.() ?? "";
+
   const storefront = createGraphqlClient({
     fetcher: fetchSafe,
     endpoint: `https://${storeName}.myshopify.com/api/2023-07/graphql.json`,
@@ -63,6 +71,7 @@ export default function App(props: Props): App<Manifest, State> {
       "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
     }),
   });
+
   const admin = createGraphqlClient({
     fetcher: fetchSafe,
     endpoint:
@@ -72,16 +81,19 @@ export default function App(props: Props): App<Manifest, State> {
       "X-Shopify-Access-Token": stringAdminAccessToken,
     }),
   });
+
   const byZipCode = (zip: string) => {
     return Promise.resolve({
       provinceCode: getStateFromZip(zip),
     });
   };
+
   return {
     state: { ...props, admin, storefront, address: { byZipCode } },
     manifest,
   };
 }
+
 export const preview = async () => {
   const markdownContent = await Markdown(
     new URL("./README.md", import.meta.url).href,
