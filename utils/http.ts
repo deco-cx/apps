9ec17bbba53e313486373f3277f4ac7c9f3263e2
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-10-01 22:35:48 -0300

    Opt keep empty paths on http client (#895)
    * Keep empty segments if needed

--- utils/http.ts
+++ utils/http.ts
@@ -76,6 +76,8 @@ export interface HttpClientOptions {
   headers?: Headers;
   processHeaders?: (headers?: Headers) => Headers | undefined;
   fetcher?: typeof fetch;
+  // Keep empty segments in the URL
+  keepEmptySegments?: boolean;
 }
 export const createHttpClient = <T>(
   {
@@ -83,6 +85,7 @@ export const createHttpClient = <T>(
     headers: defaultHeaders,
     processHeaders = (headers?: Headers) => headers,
     fetcher = fetchSafe,
+    keepEmptySegments,
   }: HttpClientOptions,
 ): ClientOf<T> => {
   // Base should always endwith / so when concatenating with path we get the right URL
@@ -122,7 +125,9 @@ export const createHttpClient = <T>(
             return param;
           })
           .filter((x) =>
-            typeof x === "string" && x.length || typeof x === "number"
+            typeof x === "string"
+              ? (keepEmptySegments || x.length)
+              : typeof x === "number"
           )
           .join("/");
         const url = new URL(compiled, base);
