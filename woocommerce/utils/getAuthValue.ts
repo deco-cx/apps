2001618851bfaf976213410f09fa0334a42c0e92
@yuri_assuncx <killerkoll2012@gmail.com>
2024-10-03 16:38:37 -0300

    chore(woocommerce): fmt

--- woocommerce/utils/getAuthValue.ts
+++ woocommerce/utils/getAuthValue.ts
@@ -1,4 +1,4 @@
 import { Secret } from "../../website/loaders/secret.ts";
 
-export const getAuthValue = (key?: Secret) => 
+export const getAuthValue = (key?: Secret) =>
   typeof key === "string" ? key : key?.get?.() ?? "";
