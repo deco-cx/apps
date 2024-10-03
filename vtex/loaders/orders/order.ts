9245e39edefba85fb45880bd7422bdb3d3916bfb
marcoferreiradev <mw.marcowilliam@gmail.com>
2024-10-03 15:06:51 -0300

    feat: add the unitary order loader and start creating its typing

---
+++ vtex/loaders/orders/order.ts
@@ -0,0 +1,35 @@
+import { RequestURLParam } from "../../../website/functions/requestToParam.ts";
+import { AppContext } from "../../mod.ts";
+import { Order } from "../../utils/types.ts";
+import { parseCookie } from "../../utils/vtexId.ts";
+
+export interface Props {
+  slug: RequestURLParam;
+}
+
+export default async function loader(
+  props: Props,
+  req: Request,
+  ctx: AppContext,
+): Promise<Order | null> {
+  const { vcsDeprecated } = ctx;
+  const { cookie } = parseCookie(req.headers, ctx.account);
+
+  const { slug } = props;
+
+  const response = await vcsDeprecated["GET /api/oms/user/orders/:orderId"](
+    { orderId: slug },
+    {
+      headers: {
+        cookie,
+      },
+    },
+  );
+
+  if (response.ok) {
+    const order = await response.json();
+    return order;
+  }
+
+  return null;
+}
