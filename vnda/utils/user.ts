b62c4b98c988ed2d96367bf5ddcca2c0d3deb151
@yuri_assuncx <killerkoll2012@gmail.com>
2024-10-10 23:17:16 -0300

    fix: remove setUserCookie function

--- vnda/utils/user.ts
+++ vnda/utils/user.ts
@@ -1,23 +1,9 @@
-import { getCookies, setCookie } from "std/http/cookie.ts";
+import { getCookies } from "std/http/cookie.ts";
 
 const AUTH_COOKIE = "client_id";
-
-const ONE_WEEK_MS = 7 * 24 * 3600 * 1_000;
 
 export const getUserCookie = (headers: Headers): string | undefined => {
   const cookies = getCookies(headers);
 
   return cookies[AUTH_COOKIE];
-};
-
-export const setUserCookie = (headers: Headers, accessToken: string) => {
-  setCookie(headers, {
-    name: AUTH_COOKIE,
-    value: accessToken,
-    path: "/",
-    expires: new Date(Date.now() + ONE_WEEK_MS),
-    httpOnly: true,
-    secure: true,
-    sameSite: "Lax",
-  });
 };
