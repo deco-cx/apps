ae18a59fb64d3b480e51688b671adbb499799bdd
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-10-16 14:27:46 -0400

    fix: isToday

--- github-bot/sdk/date.ts
+++ github-bot/sdk/date.ts
@@ -1,22 +1,15 @@
+import { dayjs } from "../deps/deps.ts";
+
 export function dateInSeconds(date: string | Date) {
   return Math.floor(
     new Date(date).getTime() / 1000,
   );
 }
 
-function getDateFromSaoPauloTZ(date: string | Date) {
-  const [day, month, year] = new Date(date)
-    .toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })
-    .split(", ")?.[0]?.split("/");
-
-  return { year, month, day };
-}
-
 export function isToday(date: string | Date) {
-  const today = getDateFromSaoPauloTZ(new Date());
-  const _date = getDateFromSaoPauloTZ(new Date(date));
+  const now = new Date(date);
+  const endOfDay = dayjs().endOf("day");
+  const startOfDay = dayjs().startOf("day");
 
-  return _date.day === today.day &&
-    today.month === _date.month &&
-    today.year === _date.year;
+  return startOfDay.isBefore(now) && endOfDay.isAfter(now);
 }
