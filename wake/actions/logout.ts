b5e5bf6720af5cb4683ed0f3ab7dc16802fe260b
decobot <capy@deco.cx>
2024-10-24 15:00:59 -0300

    :wrench: feat: remove delete checkout cookie from logout invoke

--- wake/actions/logout.ts
+++ wake/actions/logout.ts
@@ -1,6 +1,5 @@
 import type { AppContext } from "../mod.ts";
 import { deleteCookie } from "std/http/cookie.ts";
-import { CART_COOKIE } from "../utils/cart.ts";
 
 export default function (
   _props: object,
@@ -9,5 +8,4 @@ export default function (
 ) {
   deleteCookie(response.headers, "customerToken", { path: "/" });
   deleteCookie(response.headers, "fbits-login", { path: "/" });
-  deleteCookie(response.headers, CART_COOKIE, { path: "/" });
 }
