1209a698f9ea7fefe69f015f997f63d45efb7e87
guitavano <tavano62@gmail.com>
2024-11-12 17:08:09 -0300

    remove unused props

--- vtex/loaders/logistics/listSalesChannelById.ts
+++ vtex/loaders/logistics/listSalesChannelById.ts
@@ -1,21 +1,15 @@
 import type { AppContext } from "../../mod.ts";
 import { SalesChannel } from "../../utils/types.ts";
 
-interface Props {
-  id: string;
-}
-
 export default async function loader(
-  props: Props,
+  _props: unknown,
   _req: Request,
   ctx: AppContext,
 ): Promise<SalesChannel[]> {
   const { vcs } = ctx;
 
   const salesChannel = await vcs
-    ["GET /api/catalog_system/pvt/saleschannel/list"]({
-      salesChannelId: props.id,
-    })
+    ["GET /api/catalog_system/pvt/saleschannel/list"]({})
     .then((r) => r.json());
 
   return salesChannel;
