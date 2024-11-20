96f9751ed626bbe4411c3c63b3c7eaeb1dfb680a
decobot <capy@deco.cx>
2024-11-20 11:00:59 -0300

    keep compatibility

--- wake/loaders/productListingPage.ts
+++ wake/loaders/productListingPage.ts
@@ -110,6 +110,13 @@ export interface Props {
   /**
    * @title Partner Param
    * @description page param to partners page
+   * @deprecated
+   */
+  slug?: RequestURLParam;
+
+  /**
+   * @title Partner Param
+   * @description page param to partners page
    */
   partnerAlias?: RequestURLParam;
 }
@@ -140,14 +147,17 @@ const filtersFromParams = (searchParams: URLSearchParams) => {
 const searchLoader = async (
   props: Props,
   req: Request,
-  ctx: AppContext,
+  ctx: AppContext
 ): Promise<ProductListingPage | null> => {
   // get url from params
-  const url = new URL(req.url).pathname === "/live/invoke"
-    ? new URL(props.pageHref || req.headers.get("referer") || req.url)
-    : new URL(props.pageHref || req.url);
+  const url =
+    new URL(req.url).pathname === "/live/invoke"
+      ? new URL(props.pageHref || req.headers.get("referer") || req.url)
+      : new URL(props.pageHref || req.url);
 
   const { storefront } = ctx;
+
+  const partnerAlias = props.partnerAlias ?? props.slug;
 
   const partnerAccessTokenCookie = getPartnerCookie(req.headers);
 
@@ -156,40 +166,50 @@ const searchLoader = async (
   const limit = Number(url.searchParams.get("tamanho") ?? props.limit ?? 12);
 
   const filters = filtersFromParams(url.searchParams) ?? props.filters;
-  const sort = (url.searchParams.get("sort") as SortValue | null) ??
+  const sort =
+    (url.searchParams.get("sort") as SortValue | null) ??
     (url.searchParams.get("ordenacao") as SortValue | null) ??
     props.sort ??
     "SALES:DESC";
-  const page = props.page ?? Number(url.searchParams.get("page")) ??
-    Number(url.searchParams.get("pagina")) ?? 0;
+  const page =
+    props.page ??
+    Number(url.searchParams.get("page")) ??
+    Number(url.searchParams.get("pagina")) ??
+    0;
   const query = props.query ?? url.searchParams.get("busca");
   const operation = props.operation ?? "AND";
 
   const [sortKey, sortDirection] = sort.split(":") as [
     ProductSortKeys,
-    SortDirection,
+    SortDirection
   ];
 
   const onlyMainVariant = props.onlyMainVariant ?? true;
   const [minimumPrice, maximumPrice] =
-    url.searchParams.getAll("filtro")?.find((i) => i.startsWith("precoPor"))
-      ?.split(":")[1]?.split(";").map(Number) ??
-      url.searchParams.get("precoPor")?.split(";").map(Number) ?? [];
+    url.searchParams
+      .getAll("filtro")
+      ?.find((i) => i.startsWith("precoPor"))
+      ?.split(":")[1]
+      ?.split(";")
+      .map(Number) ??
+    url.searchParams.get("precoPor")?.split(";").map(Number) ??
+    [];
 
   const offset = page <= 1 ? 0 : (page - 1) * limit;
 
-  const partnerData = props.partnerAlias
-    ? await storefront.query<
-      GetPartnersQuery,
-      GetPartnersQueryVariables
-    >({
-      variables: { first: 1, alias: [props.partnerAlias] },
-      ...GetPartners,
-    }, { headers })
+  const partnerData = partnerAlias
+    ? await storefront.query<GetPartnersQuery, GetPartnersQueryVariables>(
+        {
+          variables: { first: 1, alias: [partnerAlias] },
+          ...GetPartners,
+        },
+        { headers }
+      )
     : null;
 
-  const partnerAccessToken = partnerData?.partners?.edges?.[0]?.node
-    ?.partnerAccessToken ?? partnerAccessTokenCookie;
+  const partnerAccessToken =
+    partnerData?.partners?.edges?.[0]?.node?.partnerAccessToken ??
+    partnerAccessTokenCookie;
 
   if (partnerAccessToken) {
     try {
@@ -201,14 +221,17 @@ const searchLoader = async (
     }
   }
 
-  const urlData = await storefront.query<GetUrlQuery, GetUrlQueryVariables>({
-    variables: {
-      url: url.pathname,
+  const urlData = await storefront.query<GetUrlQuery, GetUrlQueryVariables>(
+    {
+      variables: {
+        url: url.pathname,
+      },
+      ...GetURL,
     },
-    ...GetURL,
-  }, {
-    headers,
-  });
+    {
+      headers,
+    }
+  );
 
   const isHotsite = urlData.uri?.kind === "HOTSITE";
 
@@ -228,20 +251,20 @@ const searchLoader = async (
 
   const data = isHotsite
     ? await storefront.query<HotsiteQuery, HotsiteQueryVariables>({
-      variables: {
-        ...commonParams,
-        url: url.pathname,
-      },
-      ...Hotsite,
-    })
+        variables: {
+          ...commonParams,
+          url: url.pathname,
+        },
+        ...Hotsite,
+      })
     : await storefront.query<SearchQuery, SearchQueryVariables>({
-      variables: {
-        ...commonParams,
-        query,
-        operation,
-      },
-      ...Search,
-    });
+        variables: {
+          ...commonParams,
+          query,
+          operation,
+        },
+        ...Search,
+      });
 
   const products = data?.result?.productsByOffset?.items ?? [];
 
@@ -249,9 +272,8 @@ const searchLoader = async (
   const previousPage = new URLSearchParams(url.searchParams);
 
   const hasNextPage = Boolean(
-    (data?.result?.productsByOffset?.totalCount ?? 0) /
-        limit >
-      (data?.result?.productsByOffset?.page ?? 0),
+    (data?.result?.productsByOffset?.totalCount ?? 0) / limit >
+      (data?.result?.productsByOffset?.page ?? 0)
   );
 
   const hasPreviousPage = page > 1;
@@ -278,16 +300,17 @@ const searchLoader = async (
 
   const title = isHotsite
     ? (data as HotsiteQuery)?.result?.seo?.find((i) => i?.type === "TITLE")
-      ?.content
+        ?.content
     : capitalize(query || "");
   const description = isHotsite
-    ? (data as HotsiteQuery)?.result?.seo?.find((i) =>
-      i?.name === "description"
-    )?.content
+    ? (data as HotsiteQuery)?.result?.seo?.find(
+        (i) => i?.name === "description"
+      )?.content
     : capitalize(query || "");
-  const canonical =
-    new URL(isHotsite ? `/${(data as HotsiteQuery)?.result?.url}` : url, url)
-      .href;
+  const canonical = new URL(
+    isHotsite ? `/${(data as HotsiteQuery)?.result?.url}` : url,
+    url
+  ).href;
 
   return {
     "@type": "ProductListingPage",
@@ -309,8 +332,8 @@ const searchLoader = async (
     products: products
       ?.filter((p): p is ProductFragment => Boolean(p))
       .map((variant) => {
-        const productVariations = variations?.filter((v) =>
-          v.inProductGroupWithID === variant.productId
+        const productVariations = variations?.filter(
+          (v) => v.inProductGroupWithID === variant.productId
         );
 
         return toProduct(variant, { base: url }, productVariations);
