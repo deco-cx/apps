cc484ecb00b7fd5726fcc882470195808d825b16
guitavano <tavano62@gmail.com>
2024-11-22 08:48:14 -0300

    remove comment

--- vtex/loaders/payments/userPayments.ts
+++ vtex/loaders/payments/userPayments.ts
@@ -1,11 +1,3 @@
-// fetch("https://www.als.com/_v/private/graphql/v1?workspace=master&maxAge=long&appsEtag=remove&domain=store&locale=en-US&__bindingId=d8649f18-3877-43de-88e6-c45a81eddc02", {
-// 	"headers": {
-// 		"content-type": "application/json",
-// 	},
-// 	"body": "{\"operationName\":\"Payments\",\"variables\":{},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"95af11127e44f1857144e38f18635e0d085113c3bfdda3e4b8bc99ae63e14e60\",\"sender\":\"vtex.my-cards@1.x\",\"provider\":\"vtex.store-graphql@2.x\"}}}",
-// 	"method": "POST"
-// })
-
 import { AppContext } from "../../mod.ts";
 import { parseCookie } from "../../utils/vtexId.ts";
 
