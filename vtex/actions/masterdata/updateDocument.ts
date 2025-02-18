aa845c5a5fd4c181d1a481c74aa1d6181a3431d0
vitoo <32278696+vitoUwu@users.noreply.github.com>
2025-02-18 09:31:29 -0400

    feat: implement masterdata update document api (#1002)

---
+++ vtex/actions/masterdata/updateDocument.ts
@@ -0,0 +1,56 @@
+import { AppContext } from "../../mod.ts";
+import type { IdHrefDocumentID } from "../../utils/openapi/vcs.openapi.gen.ts";
+import { parseCookie } from "../../utils/vtexId.ts";
+
+export interface Props {
+  /**
+   * Unique identifier of the document to be created.
+   */
+  id: string;
+  acronym: string;
+  data: Record<string, unknown>;
+  createIfNotExists?: boolean;
+}
+
+/**
+ * @docs https://developers.vtex.com/docs/api-reference/masterdata-api#patch-/api/dataentities/-acronym-/documents?endpoint=patch-/api/dataentities/-acronym-/documents
+ * @docs https://developers.vtex.com/docs/api-reference/masterdata-api#patch-/api/dataentities/-acronym-/documents/-id-?endpoint=patch-/api/dataentities/-acronym-/documents/-id-
+ */
+const action = async (
+  props: Props,
+  req: Request,
+  ctx: AppContext,
+): Promise<unknown | IdHrefDocumentID> => {
+  const { vcs } = ctx;
+  const { id, data, acronym, createIfNotExists } = props;
+  const { cookie } = parseCookie(req.headers, ctx.account);
+
+  const requestOptions = {
+    body: createIfNotExists ? { ...data, id } : data,
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      cookie,
+    },
+  };
+
+  const response =
+    await (createIfNotExists
+      ? vcs["PATCH /api/dataentities/:acronym/documents"](
+        { acronym },
+        requestOptions,
+      )
+      : vcs["PATCH /api/dataentities/:acronym/documents/:id"](
+        { acronym, id },
+        requestOptions,
+      ));
+
+  if (response.headers.get("content-type")?.includes("application/json")) {
+    return response.json();
+  }
+
+  return;
+};
+
+export const defaultVisibility = "private";
+export default action;
