204643e27c8e1454b4f5cf627a49a596625a2832
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-02-21 03:39:43 -0400

    feat: getUserProfile loader

---
+++ vtex/loaders/profile/getUserProfile.ts
@@ -0,0 +1,83 @@
+import type { AppContext } from "../../mod.ts";
+import type { Profile } from "../../utils/types.ts";
+import { parseCookie } from "../../utils/vtexId.ts";
+
+const addressProperties = `
+addressType
+receiverName
+addressId
+postalCode
+city
+state
+country
+street
+number
+neighborhood
+complement
+reference
+geoCoordinates
+`;
+const query = `query getUserProfile {
+  profile {
+    cacheId
+    email
+    firstName
+    lastName
+    document
+    userId
+    birthDate
+    gender
+    homePhone
+    businessPhone
+    addresses { ${addressProperties} }
+    isCorporate
+    tradeName
+    corporateName
+    corporateDocument
+    stateRegistration
+    payments {
+      cacheId
+      id
+      paymentSystem
+      paymentSystemName
+      cardNumber
+      address { ${addressProperties} }
+      isExpired
+      expirationDate
+      accountStatus
+    }
+    customFields {
+      key
+      value
+    }
+    passwordLastUpdate
+  }
+}`;
+
+async function loader(
+  _props: unknown,
+  req: Request,
+  ctx: AppContext,
+): Promise<Profile | null> {
+  const { io } = ctx;
+  const { cookie, payload } = parseCookie(req.headers, ctx.account);
+
+  if (!payload?.sub || !payload?.userId) {
+    return null;
+  }
+
+  try {
+    const { profile } = await io.query<{ profile: Profile }, null>(
+      { query },
+      { headers: { cookie } },
+    );
+
+    return profile ?? null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export const defaultVisibility = "private";
+export default loader;
