85ebda8bd3a4c37660efd212f40e4941c556dae9
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-02-21 01:59:29 -0400

    feat: implement payment related APIs

---
+++ vtex/actions/payment/deletePaymentToken.ts
@@ -0,0 +1,40 @@
+import { AppContext } from "../../mod.ts";
+import { parseCookie } from "../../utils/vtexId.ts";
+
+export interface DeleteCard {
+  deletePaymentToken: boolean;
+}
+
+const mutation = `mutation DeleteCreditCardToken($tokenId: ID!) {
+  deletePaymentToken(tokenId: $tokenId) @context(provider: "vtex.my-cards-graphql@2.x")
+}`;
+
+interface Props {
+  id: string;
+}
+
+async function loader(
+  { id }: Props,
+  req: Request,
+  ctx: AppContext,
+): Promise<DeleteCard | null> {
+  const { io } = ctx;
+  const { cookie, payload } = parseCookie(req.headers, ctx.account);
+
+  if (!payload?.sub || !payload?.userId) {
+    return null;
+  }
+
+  try {
+    return await io.query<DeleteCard, { tokenId: string }>({
+      query: mutation,
+      variables: { tokenId: id },
+    }, { headers: { cookie } });
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export const defaultVisibility = "private";
+export default loader;
