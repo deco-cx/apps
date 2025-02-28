7dd0455c6e2b7f51f8f3814102b4ac54352ed59c
ryansantos <ryan.ribeiro@cadastra.com>
2025-02-28 08:44:52 -0300

    fix: update streamshop sdk

--- streamshop/sections/Stories.tsx
+++ streamshop/sections/Stories.tsx
@@ -36,6 +36,8 @@ export default function Stories({
 }: Props) {
   return (
     <>
+    <script async src="https://assets.streamshop.com.br/sdk-ads/liveshop-ads-video.min.js" ></script>
+    <script async src="https://assets.streamshop.com.br/sdk-ads/liveshop-ads-carousel.min.js" ></script>
       <Section.Container>
         <Section.Header title={title} cta={cta} />
 
@@ -52,21 +54,6 @@ export default function Stories({
           </liveshop-ads-carousel>
         </div>
       </Section.Container>
-      <script
-        async
-        src="https://assets.streamshop.com.br/sdk-ads/liveshop-ads-video.min.js"
-      >
-      </script>
-      <script
-        async
-        src="https://assets.streamshop.com.br/sdk-ads/liveshop-ads-carousel.min.js"
-      >
-      </script>
-      <script
-        async
-        src="https://assets.streamshop.com.br/sdk/liveshop-web-sdk.min.js"
-      >
-      </script>
     </>
   );
 }
