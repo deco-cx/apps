4b2e56ae0283e0ea128a9d8cab4a052a9577e873
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2025-03-03 16:48:38 -0300

    Cache on product IDs

--- vtex/loaders/intelligentSearch/productList.ts
+++ vtex/loaders/intelligentSearch/productList.ts
@@ -47,7 +47,7 @@ export interface FacetsProps extends CommonProps {
    * @description query to use on search
    * @examples "shoes"\n"blue shoes"
    */
-  query: string;
+  query?: string;
   /**
    * @title Facets string
    * @description (e.g.: 'catergory-1/moda-feminina/category-2/calcados')
@@ -246,7 +246,7 @@ const getSearchParams = (
 ): Entry[] => {
   if (isFacetsList(props)) {
     return [
-      ["query", props.query ?? searchParams.get("q")],
+      ["query", props.query ?? (searchParams.get("q") ?? "")],
       ["count", (props.count || searchParams.get("count") || 12).toString()],
       ["sort", props.sort || searchParams.get("sort") || ""],
       ["selectedFacets", props.facets],
@@ -297,8 +297,8 @@ export const cacheKey = (
 
   const url = new URL(req.url);
   if (
-    url.searchParams.has("q") ||
-    ctx.isInvoke && isProductIDList(props)
+    // Avoid cache on loader call over call and on search terms
+    url.searchParams.has("q") || ctx.isInvoke
   ) {
     return null;
   }
