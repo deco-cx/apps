59ce57d82dedd55071bfe476edcd8dbbc1a2f1ba
pedrofrxncx <pedro_vaz_franca@hotmail.com>
2025-03-27 13:26:41 -0300

    feat(vertex): update googleCredentials, location, and project properties to use Secret type

--- vertex/mod.ts
+++ vertex/mod.ts
@@ -1,19 +1,29 @@
 import { type App, type AppContext as AC } from "@deco/deco";
 import manifest, { Manifest } from "./manifest.gen.ts";
 import { createVertex } from "npm:@ai-sdk/google-vertex/edge";
+import { Secret } from "../website/loaders/secret.ts";
 
 interface State {
   vertexClient: ReturnType<typeof createVertex>;
 }
 
 interface Props {
+  /**
+   * @title Google Cloud Credentials
+   */
   googleCredentials: {
-    clientEmail: string;
-    privateKeyId: string;
-    privateKey: string;
+    clientEmail: Secret;
+    privateKeyId: Secret;
+    privateKey: Secret;
   };
-  location: string;
-  project: string;
+  /**
+   * @title Location
+   */
+  location: Secret;
+  /**
+   * @title Project
+   */
+  project: Secret;
 }
 /**
  * @title Vertex
@@ -23,10 +33,15 @@ interface Props {
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
 export default function Vertex(props: Props): App<Manifest, State> {
+  const { googleCredentials, location, project } = props;
   const vertex = createVertex({
-    googleCredentials: props.googleCredentials,
-    location: props.location,
-    project: props.project,
+    googleCredentials: {
+      clientEmail: googleCredentials.clientEmail.get(),
+      privateKeyId: googleCredentials.privateKeyId.get(),
+      privateKey: googleCredentials.privateKey.get(),
+    },
+    location: location.get(),
+    project: project.get(),
   });
 
   return {
