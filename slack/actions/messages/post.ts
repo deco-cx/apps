5ccd707490c4ab3096b33bcfa8d4055df101f52e
Marcos Candeia <marrcooos@gmail.com>
2025-03-27 20:39:40 -0300

    Add slack app
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ slack/actions/messages/post.ts
@@ -0,0 +1,28 @@
+import type { AppContext } from "../../mod.ts";
+import type { SlackMessage, SlackResponse } from "../../client.ts";
+
+export interface Props {
+  /**
+   * @description The ID of the channel to post to
+   */
+  channelId: string;
+  /**
+   * @description The message text to post
+   */
+  text: string;
+}
+
+/**
+ * @title Post Message
+ * @description Posts a new message to a Slack channel and returns the message details
+ */
+export default async function postMessage(
+  props: Props,
+  _req: Request,
+  ctx: AppContext,
+): Promise<
+  SlackResponse<{ channel: string; ts: string; message: SlackMessage }>
+> {
+  const { channelId, text } = props;
+  return await ctx.slack.postMessage(channelId, text);
+}
