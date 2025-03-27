ae2aa3c13b4b0c080aec5e7e1c1cfd2eb2646c2a
Luis Sousa <lv-ss@hotmail.com>
2025-03-27 11:49:11 -0300

    fix: set cookie for apex domain

--- magento/utils/utils.ts
+++ magento/utils/utils.ts
@@ -1,12 +1,44 @@
 import { getSetCookies, setCookie } from "std/http/cookie.ts";
 import { SESSION_COOKIE } from "./constants.ts";
 
+function getApexDomain(url: string | URL) {
+  // Cria um objeto URL para analisar a URL
+  const parsedUrl = new URL(url);
+
+  // Extrai o hostname da URL (exclui o protocolo, como http://)
+  const hostname = parsedUrl.hostname;
+
+  // Divide o hostname em partes
+  const parts = hostname.split(".");
+
+  // Se o domínio tem mais de dois níveis, pega os dois últimos (domínio + TLD)
+  if (parts.length > 2) {
+    const lastTopLevelDomain = parts[parts.length - 1];
+    if (lastTopLevelDomain.length > 2) {
+      return parts.slice(parts.length - 2).join(".");
+    }
+
+    return parts.slice(parts.length - 3).join(".");
+  }
+
+  // Caso contrário, retorna o hostname inteiro (ex: "example.com")
+  return hostname;
+}
+
 export const proxySetCookie = (
   from: Headers,
   to: Headers,
   toDomain?: URL | string,
 ) => {
-  const newDomain = toDomain && new URL(toDomain);
+  let newDomain: string | undefined = undefined;
+
+  if (toDomain) {
+    if (toDomain.toString().includes("localhost")) {
+      newDomain = "localhost";
+    } else {
+      newDomain = `.${getApexDomain(toDomain)}`;
+    }
+  }
 
   for (const cookie of getSetCookies(from)) {
     if (cookie.name === SESSION_COOKIE) {
@@ -15,7 +47,7 @@ export const proxySetCookie = (
     const newCookie = newDomain
       ? {
         ...cookie,
-        domain: newDomain.hostname,
+        domain: newDomain,
       }
       : cookie;
 
