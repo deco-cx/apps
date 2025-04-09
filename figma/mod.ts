ac653fe9eafb0b776294b4cd48b74f033e848039
soutofernando <fernandosoutol45@gmail.com>
2025-04-09 13:23:39 -0300

    chore: change figma logo

--- figma/mod.ts
+++ figma/mod.ts
@@ -20,6 +20,7 @@ export type AppContext = FnContext<State, Manifest>;
  * @name FIGMA
  * @title Figma
  * @description A Deco app to interact with the Figma APIs with strongly typed responses
+ * @logo https://ih1.redbubble.net/image.4053581522.4386/raf,360x360,075,t,fafafa:ca443f4786.jpg
  */
 export default function App(props: Props): App<Manifest, State> {
   const figma = new FigmaClient(
