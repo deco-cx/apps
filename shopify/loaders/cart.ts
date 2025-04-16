83ee56c2cf2e771b62958db1498593b921ef092d
yuriassuncx <killerkoll2012@gmail.com>
2025-04-16 14:41:22 -0300

    feat: update GetCart and ListProducts queries to include languageCode and countryCode for localization

--- shopify/loaders/cart.ts
+++ shopify/loaders/cart.ts
@@ -2,17 +2,36 @@ import { AppContext } from "../mod.ts";
 import { getCartCookie, setCartCookie } from "../utils/cart.ts";
 import { CreateCart, GetCart } from "../utils/storefront/queries.ts";
 import {
+  CountryCode,
   CreateCartMutation,
   CreateCartMutationVariables,
   GetCartQuery,
   GetCartQueryVariables,
+  LanguageCode,
 } from "../utils/storefront/storefront.graphql.gen.ts";
+import { LanguageContextArgs } from "../utils/types.ts";
+
+export interface Props {
+  /**
+   * @title Language Code
+   * @description Language code for the storefront API
+   * @example "EN" for English, "FR" for French, etc.
+   */
+  languageCode?: LanguageCode;
+  /**
+   * @title Country Code
+   * @description Country code for the storefront API
+   * @example "US" for United States, "FR" for France, etc.
+   */
+  countryCode?: CountryCode;
+}
 
 const loader = async (
-  _props: unknown,
+  props: Props,
   req: Request,
   ctx: AppContext,
 ): Promise<GetCartQuery["cart"]> => {
+  const { languageCode = "PT", countryCode = "BR" } = props;
   const { storefront } = ctx;
   const maybeCartId = getCartCookie(req.headers);
 
@@ -25,8 +44,11 @@ const loader = async (
     throw new Error("Missing cart id");
   }
 
-  const cart = await storefront.query<GetCartQuery, GetCartQueryVariables>({
-    variables: { id: decodeURIComponent(cartId) },
+  const cart = await storefront.query<
+    GetCartQuery,
+    GetCartQueryVariables & LanguageContextArgs
+  >({
+    variables: { id: decodeURIComponent(cartId), languageCode, countryCode },
     ...GetCart,
   }).then((data) => data.cart);
 
