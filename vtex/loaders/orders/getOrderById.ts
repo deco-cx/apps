52143ef8ab83b1dfffd35c64224f2bdb406a65bb
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-04-17 13:35:45 -0400

    feat: add order ownership check

--- vtex/loaders/orders/getOrderById.ts
+++ vtex/loaders/orders/getOrderById.ts
@@ -1,3 +1,4 @@
+import { HttpError } from "@deco/deco";
 import { AppContext } from "../../mod.ts";
 import { Userorderdetails } from "../../utils/openapi/vcs.openapi.gen.ts";
 import { parseCookie } from "../../utils/vtexId.ts";
@@ -14,6 +15,11 @@ export default async function loader(
   const { vcs } = ctx;
   const { orderId } = props;
   const { cookie, payload } = parseCookie(req.headers, ctx.account);
+
+  const user = await ctx.invoke.vtex.loaders.user();
+  if (!user) {
+    throw new HttpError(new Response("Unauthorized", { status: 403 }));
+  }
 
   const response = await vcs["GET /api/oms/user/orders/:orderId"](
     {
@@ -32,5 +38,12 @@ export default async function loader(
   }
 
   const order = await response.json();
+
+  if (order.clientProfileData?.email !== user.email) {
+    throw new HttpError(new Response("Unauthorized", { status: 403 }));
+  }
+
   return order;
 }
+
+export const defaultVisibility = "private";
