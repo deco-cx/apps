a088d1155a884b2cb44ab5458a0dc45580f4ea83
Fernando Souto <90011575+soutofernando@users.noreply.github.com>
2025-04-22 18:34:53 -0300

    chore: update version (#1116)

--- figma/mod.ts
+++ figma/mod.ts
@@ -19,7 +19,7 @@ export type AppContext = FnContext<State, Manifest>;
 /**
  * @name Figma
  * @title Figma
- * @description A Deco app to interact with the Figma APIs with strongly typed responses
+ * @description A Deco app to interact with the Figma APIs with strongly typed responses.
  * @logo https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png
  */
 export default function App(props: Props): App<Manifest, State> {
