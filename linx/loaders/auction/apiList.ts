55a175f8eaf338635eece4ed3c3bcab4bfeebe77
marcoferreiradev <mw.marcowilliam@gmail.com>
2025-04-24 19:55:28 -0300

    refactor(linx): mockup cache key

--- linx/loaders/auction/apiList.ts
+++ linx/loaders/auction/apiList.ts
@@ -31,12 +31,10 @@ const loader = async (
   return response;
 };
 
-// export const cache = "stale-while-revalidate";
+export const cache = "stale-while-revalidate";
 
-// export const cacheKey = (_props: unknown, req: Request, _ctx: AppContext) => {
-//   const url = new URL(req.url);
-//   url.pathname = "/v1/Catalog/API.svc/web/SearchProductAuctions";
-//   return url.href;
-// };
+export const cacheKey = () => {
+  return "/v1/Catalog/API.svc/web/SearchProductAuctions";
+};
 
 export default loader;
