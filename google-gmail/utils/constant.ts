fa2523a32a3d68693d0f1f1fc02a9e2390f3ec5e
viktormarinho <viktormpcs@gmail.com>
2025-06-09 19:13:35 -0300

    fix

--- google-gmail/utils/constant.ts
+++ google-gmail/utils/constant.ts
@@ -1,4 +1,7 @@
+import { GOOGLE_USER_INFO_SCOPES } from "../../mcp/utils/google/userInfo.ts";
+
 export const SCOPES = [
+  ...GOOGLE_USER_INFO_SCOPES,
   "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/gmail.send",
 ];
