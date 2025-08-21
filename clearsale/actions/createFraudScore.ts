d6efc0ef771445111edee69421ea9914dbcf60cf
gimenes <tlgimenes@gmail.com>
2025-08-21 12:30:56 -0300

    arrays are not supported by mcp

--- clearsale/actions/createFraudScore.ts
+++ clearsale/actions/createFraudScore.ts
@@ -17,7 +17,7 @@ const action = async (
   props: Props,
   _req: Request,
   ctx: AppContext,
-): Promise<Score[]> => {
+): Promise<{ scores: Score[] }> => {
   const { transactionId } = props;
 
   // First, authenticate to get the token
@@ -31,7 +31,7 @@ const action = async (
   );
 
   const result = await response.json();
-  return result;
+  return { scores: result };
 };
 
 export default action;
