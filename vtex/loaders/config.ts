f6fa34c35ba674900129c48dad18e74aa8d572d9
marcoferreiradev <mw.marcowilliam@gmail.com>
2025-09-25 15:16:57 -0300

    feat(vtex): add subscription API client support in loader config

--- vtex/loaders/config.ts
+++ vtex/loaders/config.ts
@@ -6,6 +6,7 @@ import { type OpenAPI as API } from "../utils/openapi/api.openapi.gen.ts";
 import { type OpenAPI as VCS } from "../utils/openapi/vcs.openapi.gen.ts";
 import { type OpenAPI as MY } from "../utils/openapi/my.openapi.gen.ts";
 import { type OpenAPI as VPAY } from "../utils/openapi/payments.openapi.gen.ts";
+import { type OpenAPI as SUB } from "../utils/openapi/subscriptions.openapi.gen.ts";
 
 export type Config = {
   sp: ReturnType<typeof createHttpClient<SP>>;
@@ -14,6 +15,7 @@ export type Config = {
   my: ReturnType<typeof createHttpClient<MY>>;
   api: ReturnType<typeof createHttpClient<API>>;
   vpay: ReturnType<typeof createHttpClient<VPAY>>;
+  sub: ReturnType<typeof createHttpClient<SUB>>;
 };
 
 /**
@@ -27,6 +29,7 @@ const loader = (_props: unknown, _req: Request, ctx: AppContext): Config => ({
   vcs: ctx.vcs,
   api: ctx.api,
   vpay: ctx.vpay,
+  sub: ctx.sub,
 });
 
 export default loader;
