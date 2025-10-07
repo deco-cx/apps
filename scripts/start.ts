0368fc3bf4abfc58f73d08c6b46b85f80ec6df16
guitavano <tavano62@gmail.com>
2025-10-07 11:55:28 -0300

    fix start.ts

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,14 +1,14 @@
-// deno-lint-ignore-file no-explicit-any no-import-prefix
-import "npm:@graphql-codegen/add";
-import "npm:@graphql-codegen/schema-ast";
-import "npm:@graphql-codegen/typescript";
-import "npm:@graphql-codegen/typescript-operations";
+// deno-lint-ignore-file no-explicit-any
+import "npm:@graphql-codegen@5.0.0/add";
+import "npm:@graphql-codegen@5.0.0/schema-ast";
+import "npm:@graphql-codegen@5.0.0/typescript";
+import "npm:@graphql-codegen@5.0.0/typescript-operations";
 
 import { setupGithooks } from "https://deno.land/x/githooks@0.0.4/githooks.ts";
-import { CodegenConfig, generate } from "npm:@graphql-codegen/cli";
-import camel from "npm:camelcase";
-import { compile } from "npm:json-schema-to-typescript";
-import { OpenAPIV3 } from "npm:openapi-types";
+import { CodegenConfig, generate } from "npm:@graphql-codegen@5.0.0/cli";
+import camel from "npm:camelcase@8.0.0";
+import { compile } from "npm:json-schema-to-typescript@15.0.4";
+import { OpenAPIV3 } from "npm:openapi-types@12.1.3";
 import { walk } from "std/fs/mod.ts";
 import { dirname, join } from "std/path/mod.ts";
 import { basename } from "std/path/win32.ts";
@@ -257,7 +257,7 @@ const generateGraphQL = async () => {
   }
 };
 
-const generateDeco = () => import("jsr:@deco/deco/scripts/bundle");
+const generateDeco = () => import("@deco/deco/scripts/bundle");
 
 await generateOpenAPI();
 await generateGraphQL();
