5006272e224521ebe81e08a1edd09e3870c042cf
guitavano <tavano62@gmail.com>
2025-11-06 11:04:24 -0300

    fix protocol

--- website/handlers/image.ts
+++ website/handlers/image.ts
@@ -3,7 +3,7 @@ export default function Image(){
     return async (req: Request) => {
         const url = new URL(req.url);
         const lastPath = url.pathname.split("/_d/assets/")[1];
-        const imageUrl = `https://${url.host}/live/invoke/website/loaders/image.ts?src=${lastPath}&${url.searchParams.toString()}`;
+        const imageUrl = `${url.protocol}//${url.host}/live/invoke/website/loaders/image.ts?src=${lastPath}&${url.searchParams.toString()}`;
         const response = await fetch(imageUrl);
         return new Response(response.body, response);
     }
