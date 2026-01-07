528ae7c878d5f6422030a521b7aa442a81eb8a46
guitavano <tavano62@gmail.com>
2026-01-07 16:44:20 -0300

    optmize magento

--- website/components/Image.tsx
+++ website/components/Image.tsx
@@ -114,6 +114,19 @@ const optimizeWake = (opts: OptimizationOptions) => {
   return url.href;
 };
 
+const optimizeMagento = (opts: OptimizationOptions) => {
+  const { originalSrc, width, height } = opts;
+
+  const url = new URL(originalSrc);
+  url.searchParams.set("width", `${width}`);
+  url.searchParams.set("height", `${height}`);
+  url.searchParams.set("canvas", `${width}:${height}`);
+  url.searchParams.set("optimize", "low");
+  url.searchParams.set("fit", opts.fit === "cover" ? "" : "bounds");
+
+  return url.href;
+}
+
 export const getOptimizedMediaUrl = (opts: OptimizationOptions) => {
   const { originalSrc, width, height, fit } = opts;
 
@@ -122,6 +135,11 @@ export const getOptimizedMediaUrl = (opts: OptimizationOptions) => {
   }
 
   if (!isImageOptmizationEnabled()) {
+
+    if (originalSrc.includes("media/catalog/product")) {
+      return optimizeMagento(opts);
+    }
+
     if (originalSrc.includes("fbitsstatic.net/img/")) {
       return optimizeWake(opts);
     }
