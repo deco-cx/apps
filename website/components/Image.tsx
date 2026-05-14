c4525bb6ed1e44c82ab83d047fcc8efc61a30958
Dex <fernandofrizzatti@gmail.com>
2026-05-14 10:47:24 -0300

    Update Image.tsx

--- website/components/Image.tsx
+++ website/components/Image.tsx
@@ -78,7 +78,7 @@ export type QualityOptions = "low" | "medium" | "high" | "original"; // 60% - 70
 export type DefaultQualityOptions = "low" | "medium" | "high";
 
 export const DefaultImageQualityContext = createContext<
-  DefaultQualityOptions | undefined
+  QualityOptions | undefined
 >(undefined);
 
 interface OptimizationOptions {
